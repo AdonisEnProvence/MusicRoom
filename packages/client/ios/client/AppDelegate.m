078413f5e6bdd2aad7d694a73e77efd9ee2f98d1
Devessier <baptiste@devessier.fr>
2021-05-21 19:54:45 +0200

    feat(client): install react native spotify remote lib and configure it for iOS

--- packages/client/ios/client/AppDelegate.m
+++ packages/client/ios/client/AppDelegate.m
@@ -4,6 +4,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <RNSpotifyRemote.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -57,6 +59,11 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)URL options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
+{
+  return [[RNSpotifyRemoteAuth sharedInstance] application:application openURL:URL options:options];
 }
 
 @end

80a0152ae27431dcaa0b310cca1f355bb1db5530
prastoin <tkesray@gmail.com>
2021-05-27 17:53:11 +0200

    feat(client): init dripsy global theme

--- packages/client/App.tsx
+++ packages/client/App.tsx
@@ -1,10 +1,46 @@
+import { DripsyProvider } from 'dripsy';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+export type SizeTerms = 's' | 'm' | 'l' | 'xl';
+export type BackgroundTerms = 'primary' | 'seconday' | 'tertiary';
+
+const theme = {
+    colors: {
+        primary: '#212922',
+        secondary: '#294936',
+        tertiary: '#5B8266',
+        text: '#626262',
+        background: '#fff',
+        success: 'gold',
+    },
+    space: {
+        s: 4,
+        m: 8,
+        l: 16,
+        xl: 24,
+    },
+    borderWidths: {
+        s: 1,
+        m: 2,
+        l: 3,
+    },
+    fontSizes: {
+        s: 16,
+        m: 20,
+        l: 24,
+        xl: 32,
+    },
+    radii: {
+        s: 5,
+        m: 10,
+        l: 15,
+    },
+};
 
 const App: React.FC = () => {
     const isLoadingComplete = useCachedResources();
@@ -14,10 +50,12 @@ const App: React.FC = () => {
         return null;
     } else {
         return (
-            <SafeAreaProvider>
-                <Navigation colorScheme={colorScheme} />
-                <StatusBar />
-            </SafeAreaProvider>
+            <DripsyProvider theme={theme}>
+                <SafeAreaProvider>
+                    <Navigation colorScheme={colorScheme} />
+                    <StatusBar />
+                </SafeAreaProvider>
+            </DripsyProvider>
         );
     }
 };

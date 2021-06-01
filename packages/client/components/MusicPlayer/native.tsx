9d1d9313404bf9c5844eac72edee0828f5b9b66b
Devessier <baptiste@devessier.fr>
2021-06-01 11:13:11 +0200

    feat: create a component that is different according to the platform the application runs on
    We use `Platform.select` to dynamically load the component we want.

---
+++ packages/client/components/MusicPlayer/native.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Player: React.FC = () => {
+    return (
+        <View>
+            <Text>On Native</Text>
+        </View>
+    );
+};
+
+export default Player;
--- packages/client/screens/HomeScreen.tsx
+++ packages/client/screens/HomeScreen.tsx
@@ -3,12 +3,17 @@ import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import Block from '../components/template/Block';
 import Title from '../components/template/Title';
+import Player from '../components/MusicPlayer';
 
 const HomeScreen: React.FC = () => {
     const navigation = useNavigation();
+
     return (
         <Block background={'primary'}>
             <Title>This is a basic home</Title>
+
+            <Player />
+
             <Button
                 title="Go settings"
                 onPress={() => navigation.navigate('Settings')}

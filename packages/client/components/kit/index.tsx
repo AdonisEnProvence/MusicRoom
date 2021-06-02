055079a6fbea5e69559d5ffd4c94b58fb2c12e4f
Devessier <baptiste@devessier.fr>
2021-06-02 16:18:42 +0200

    feat(client): export all kit components from kit/index.tsx
    It will be easier to pick another component like that.

---
+++ packages/client/components/kit/index.tsx
@@ -0,0 +1,19 @@
+import Block from './Block';
+import FlexColContainer from './FlexColContainer';
+import FlexRowContainer from './FlexRowContainer';
+import Hr from './Hr';
+import MSFlatList from './MSFlatList';
+import TextInput from './TextInput';
+import Title from './Title';
+import Typo from './Typo';
+
+export {
+    Block,
+    FlexColContainer,
+    FlexRowContainer,
+    Hr,
+    MSFlatList,
+    TextInput,
+    Title,
+    Typo,
+};

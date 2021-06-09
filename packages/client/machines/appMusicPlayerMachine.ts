db992883ac6db8961386008e71b7ff67868e6e5d
Devessier <baptiste@devessier.fr>
2021-06-09 18:15:47 +0200

    feat(client): assign room information to context when we receive server confirmation

--- packages/client/machines/appMusicPlayerMachine.ts
+++ packages/client/machines/appMusicPlayerMachine.ts
@@ -32,7 +32,7 @@ interface CreateAppMusicPlayerMachineArgs {
     socket: Socket<RoomServerToClientEvents, RoomClientToServerEvents>;
 }
 
-function joinRoomCallback(sendBack: Sender<AppMusicPlayerMachineEvent>) {
+function joiningRoomCallback(sendBack: Sender<AppMusicPlayerMachineEvent>) {
     return (roomID: string, name: string) => {
         console.log(roomID);
         sendBack({
@@ -56,124 +56,135 @@ export const createAppMusicPlayerMachine = ({
     any,
     AppMusicPlayerMachineEvent
 > =>
-    createMachine<AppMusicPlayerMachineContext, AppMusicPlayerMachineEvent>({
-        invoke: {
-            src: (_context, _event) => (sendBack) => {
-                socket.on('JOIN_ROOM_CALLBACK', ({ roomID, name }) => {
-                    console.log(
-                        `J'AI BIEN RECU MON FIX AUJDH MAIS JE VAIS EN PRENDRE UN DEUXIEME CE SOIR`,
-                        roomID,
-                    );
-                    sendBack({
-                        type: 'JOINED_ROOM',
-                        room: {
-                            name: name,
-                            roomID,
-                        },
-                        track: {
-                            artistName: 'artistName', //TODO
-                            name: 'name', //TODO
-                        },
+    createMachine<AppMusicPlayerMachineContext, AppMusicPlayerMachineEvent>(
+        {
+            invoke: {
+                src: (_context, _event) => (sendBack) => {
+                    socket.on('JOIN_ROOM_CALLBACK', ({ roomID, name }) => {
+                        sendBack({
+                            type: 'JOINED_ROOM',
+                            room: {
+                                name: name,
+                                roomID,
+                            },
+                            track: {
+                                artistName: 'artistName', //TODO
+                                name: 'name', //TODO
+                            },
+                        });
                     });
-                });
-            },
-        },
-        context: {
-            currentRoom: undefined,
-            currentTrack: undefined,
-            waitingRoomID: undefined,
-        },
-
-        initial: 'waitingJoiningRoom',
-
-        states: {
-            waitingJoiningRoom: {
-                on: {
-                    CREATE_ROOM: {
-                        target: 'connectingToRoom',
-                    },
-                    JOIN_ROOM: {
-                        target: 'joinRoom',
-                        actions: assign((context, event) => {
-                            if (event.type !== 'JOIN_ROOM') {
-                                return context;
-                            }
-
-                            return {
-                                ...context,
-                                waitingRoomID: event.roomID,
-                            };
-                        }),
-                    },
                 },
             },
 
-            connectingToRoom: {
-                invoke: {
-                    src: (_context, event) => (sendBack) => {
-                        if (event.type !== 'CREATE_ROOM') {
-                            throw new Error(
-                                'Service must be called in reaction to CREATE_ROOM event',
-                            );
-                        }
-                        const payload = {
-                            userID: 'user1',
-                            name: 'your_room_name',
-                        };
-                        socket.emit(
-                            'CREATE_ROOM',
-                            payload,
-                            joinRoomCallback(sendBack),
-                        );
-                    },
-                },
-
-                on: {
-                    JOINED_ROOM: {
-                        target: 'connectedToRoom',
-                        actions: assign((context, event) => {
-                            if (event.type !== 'JOINED_ROOM') {
-                                return context;
-                            }
-
-                            return {
-                                ...context,
-                                currentRoom: event.room,
-                                currentTrack: event.track,
-                            };
-                        }),
-                    },
-                },
+            context: {
+                currentRoom: undefined,
+                currentTrack: undefined,
+                waitingRoomID: undefined,
             },
-            joinRoom: {
-                invoke: {
-                    src: (_context, event) => (sendBack) => {
-                        if (event.type !== 'JOIN_ROOM') {
-                            throw new Error(
-                                'Service must be called in reaction to JOIN_ROOM event',
-                            );
-                        }
-                        console.log('POUIOUIUIPOUIIOPUOIPUOIU', event);
-                        const payload = {
-                            roomID: event.roomID,
-                            userID: 'user2',
-                        };
-                        socket.emit('JOIN_ROOM', payload);
-                    },
-                },
-                on: {
-                    JOINED_ROOM: {
-                        target: 'connectedToRoom',
-                        cond: (context, event) => {
-                            return event.room.roomID === context.waitingRoomID;
+
+            initial: 'waitingJoiningRoom',
+
+            states: {
+                waitingJoiningRoom: {
+                    on: {
+                        CREATE_ROOM: {
+                            target: 'connectingToRoom',
+                        },
+
+                        JOIN_ROOM: {
+                            target: 'joiningRoom',
+                            actions: assign((context, event) => {
+                                if (event.type !== 'JOIN_ROOM') {
+                                    return context;
+                                }
+
+                                return {
+                                    ...context,
+                                    waitingRoomID: event.roomID,
+                                };
+                            }),
                         },
                     },
                 },
-            },
-            connectedToRoom: {
-                on: {
-                    JOIN_ROOM: 'joinRoom',
+
+                connectingToRoom: {
+                    invoke: {
+                        src: (_context, event) => (sendBack) => {
+                            if (event.type !== 'CREATE_ROOM') {
+                                throw new Error(
+                                    'Service must be called in reaction to CREATE_ROOM event',
+                                );
+                            }
+                            const payload = {
+                                userID: 'user1',
+                                name: 'your_room_name',
+                            };
+                            socket.emit(
+                                'CREATE_ROOM',
+                                payload,
+                                joiningRoomCallback(sendBack),
+                            );
+                        },
+                    },
+
+                    on: {
+                        JOINED_ROOM: {
+                            target: 'connectedToRoom',
+                            actions: 'assignRoomInformationToContext',
+                        },
+                    },
+                },
+
+                joiningRoom: {
+                    invoke: {
+                        src: (_context, event) => (sendBack) => {
+                            if (event.type !== 'JOIN_ROOM') {
+                                throw new Error(
+                                    'Service must be called in reaction to JOIN_ROOM event',
+                                );
+                            }
+                            console.log('POUIOUIUIPOUIIOPUOIPUOIU', event);
+                            const payload = {
+                                roomID: event.roomID,
+                                userID: 'user2',
+                            };
+                            socket.emit('JOIN_ROOM', payload);
+                        },
+                    },
+
+                    on: {
+                        JOINED_ROOM: {
+                            target: 'connectedToRoom',
+                            cond: (context, event) => {
+                                return (
+                                    event.room.roomID === context.waitingRoomID
+                                );
+                            },
+                            actions: 'assignRoomInformationToContext',
+                        },
+                    },
+                },
+
+                connectedToRoom: {
+                    on: {
+                        JOIN_ROOM: 'joiningRoom',
+                    },
                 },
             },
         },
-    });
+        {
+            actions: {
+                assignRoomInformationToContext: assign((context, event) => {
+                    if (event.type !== 'JOINED_ROOM') {
+                        return context;
+                    }
+
+                    return {
+                        ...context,
+                        currentRoom: event.room,
+                        currentTrack: event.track,
+                    };
+                }),
+            },
+        },
+    );

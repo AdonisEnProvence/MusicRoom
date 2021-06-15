3b6a1b5522540c69cd9c529bc1bc4d1f23d483c6
prastoin <tkesray@gmail.com>
2021-06-15 11:19:36 +0200

    feat(server): create rooms migration and model

---
+++ packages/server/app/Models/Room.ts
@@ -0,0 +1,16 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
+import { DateTime } from 'luxon';
+
+export default class Room extends BaseModel {
+    @column({ isPrimary: true })
+    public uuid: string;
+
+    @column({ columnName: 'run_id' })
+    public runID: string;
+
+    @column.dateTime({ autoCreate: true })
+    public createdAt: DateTime;
+
+    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    public updatedAt: DateTime;
+}

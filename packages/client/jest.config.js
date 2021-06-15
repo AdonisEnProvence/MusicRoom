b01837112e5e1a73630b201676028897d4431a47
Devessier <baptiste@devessier.fr>
2021-06-15 17:25:33 +0200

    test(client): mock react-native-youtube-iframe realistically and call extend-expect

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -4,5 +4,9 @@ module.exports = {
         'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|dripsy|@dripsy/.*|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
     ],
     testPathIgnorePatterns: ['/node_modules/', 'dist'],
-    setupFilesAfterEnv: ['./jest.setup.ts', './tests/global-mocks.ts'],
+    setupFilesAfterEnv: [
+        './jest.setup.tsx',
+        './tests/global-mocks.ts',
+        '@testing-library/jest-native/extend-expect',
+    ],
 };

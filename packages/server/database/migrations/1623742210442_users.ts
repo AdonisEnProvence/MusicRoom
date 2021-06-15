257cd7838314cebe210f879dd347bf270464ecc5
prastoin <tkesray@gmail.com>
2021-06-15 11:24:35 +0200

    fix(server): type issue on users migration uuid

--- packages/server/database/migrations/1623742210442_users.ts
+++ packages/server/database/migrations/1623742210442_users.ts
@@ -5,7 +5,7 @@ export default class Users extends BaseSchema {
 
     public up(): void {
         this.schema.createTable(this.tableName, (table) => {
-            table.increments('uuid').primary();
+            table.uuid('uuid').notNullable().primary();
             table.string('nickname').unique().notNullable();
             table.timestamps(true, true);
         });

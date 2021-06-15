4f19109e379c1637a39ca310a4d11d23e0059f71
Devessier <baptiste@devessier.fr>
2021-06-15 15:18:30 +0200

    test(client): use @mswjs/data to create a database and test with fake data

--- packages/client/__tests__/SearchTrackScreen.test.tsx
+++ packages/client/__tests__/SearchTrackScreen.test.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { render, fireEvent, waitFor } from '../tests/tests-utils';
 import { RootNavigator } from '../navigation';
 import { NavigationContainer } from '@react-navigation/native';
+import { db } from '../tests/data';
 
 function noop() {
     return undefined;
 }
 
-test(`Goes to Search a Track screen, searches a track and sees search results`, async () => {
-    const { getByText, getByPlaceholderText } = render(
-        <NavigationContainer>
-            <RootNavigator colorScheme="dark" toggleColorScheme={noop} />
-        </NavigationContainer>,
-    );
+test(`Goes to Search a Track screen, searches a track, sees search results, presses a song and listens to it`, async () => {
+    const fakeTrack = db.tracks.create();
 
-    expect(getByText(/this.*is.*home/i)).toBeTruthy();
+    const { getByText, getByPlaceholderText, getAllByText, findByText, debug } =
+        render(
+            <NavigationContainer>
+                <RootNavigator colorScheme="dark" toggleColorScheme={noop} />
+            </NavigationContainer>,
+        );
+
+    expect(getAllByText(/home/i).length).toBeGreaterThanOrEqual(1);
 
     const searchScreenLink = getByText(/search/i);
     expect(searchScreenLink).toBeTruthy();
@@ -26,7 +30,7 @@ test(`Goes to Search a Track screen, searches a track and sees search results`, 
     const searchInput = getByPlaceholderText(/search.*track/i);
     expect(searchInput).toBeTruthy();
 
-    const SEARCH_QUERY = 'Benjamin Biolay';
+    const SEARCH_QUERY = fakeTrack.title.slice(0, 3);
 
     /**
      * To simulate a real interaction with a text input, we need to:
@@ -39,4 +43,6 @@ test(`Goes to Search a Track screen, searches a track and sees search results`, 
     fireEvent(searchInput, 'submitEditing');
 
     await waitFor(() => expect(getByText(/results/i)).toBeTruthy());
+    const trackResultListItem = await findByText(fakeTrack.title);
+    expect(trackResultListItem).toBeTruthy();
 });

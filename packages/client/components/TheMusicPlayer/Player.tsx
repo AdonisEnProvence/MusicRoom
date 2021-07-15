bfa3e57ed595933e2276a29ad380d415d90dd963
Devessier <baptiste@devessier.fr>
2021-07-15 16:22:23 +0200

    feat(client): create a wrapper component to use react-youtube on web

--- packages/client/components/TheMusicPlayer/Player.tsx
+++ packages/client/components/TheMusicPlayer/Player.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Pressable, View } from 'react-native';
-import YoutubePlayer, { YoutubeIframeRef } from 'react-native-youtube-iframe';
+import YouTubePlayer, { PlayerRef } from '../YouTubePlayer';
 
-export type MusicPlayerRef = YoutubeIframeRef | null;
+export type MusicPlayerRef = PlayerRef | null;
 
 type MusicPlayerProps = {
     videoId: string;
@@ -23,18 +23,14 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({
     setPlayerRef,
     onTrackReady,
 }) => {
-    const playerRef = setPlayerRef as unknown as any;
-
     return (
         <Pressable onPress={noop} onLongPress={noop}>
             <View pointerEvents="none">
-                <YoutubePlayer
-                    ref={playerRef}
+                <YouTubePlayer
+                    ref={setPlayerRef}
                     height={playerHeight}
-                    play={videoState === 'playing'}
+                    playing={videoState === 'playing'}
                     videoId={videoId}
-                    //FIX for android see https://stackoverflow.com/questions/63171131/when-rendering-iframes-with-html-android-crashes-while-navigating-back-to-s
-                    webViewStyle={{ opacity: 0.99 }}
                     onReady={onTrackReady}
                 />
             </View>
---
+++ packages/client/components/YouTubePlayer/web.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useRef } from 'react';
+import { useImperativeHandle } from 'react';
+import { useEffect } from 'react';
+import { forwardRef } from 'react';
+import YouTube, { Options } from 'react-youtube';
+import { PlayerComponent, PlayerProps, PlayerRef } from './contract';
+import { YoutubeIframePlayer } from './youtube-iframe';
+
+const WebPlayer: PlayerComponent = forwardRef<PlayerRef, PlayerProps>(
+    ({ width, height, videoId, playing, onReady }, ref) => {
+        const playerRef = useRef<YoutubeIframePlayer>();
+
+        useImperativeHandle(ref, () => ({
+            getDuration() {
+                const duration = playerRef.current?.getDuration();
+                if (duration === undefined) {
+                    throw new Error(
+                        'Could not get duration from react-youtube',
+                    );
+                }
+
+                return Promise.resolve(duration);
+            },
+
+            async getCurrentTime() {
+                const currentTime = playerRef.current?.getCurrentTime();
+                if (currentTime === undefined) {
+                    throw new Error(
+                        'Could not get current time from react-native-youtube-iframe',
+                    );
+                }
+
+                return Promise.resolve(currentTime);
+            },
+        }));
+
+        useEffect(() => {
+            if (playing === true) {
+                playerRef.current?.playVideo();
+            } else {
+                playerRef.current?.pauseVideo();
+            }
+        }, [playing, playerRef]);
+
+        const playerOptions: Options = {
+            height: String(height),
+            width: width !== undefined ? String(width) : '100%',
+        };
+
+        function setPlayerRef(ref: YouTube) {
+            if (ref === null) {
+                return;
+            }
+
+            playerRef.current = ref.getInternalPlayer() as YoutubeIframePlayer;
+        }
+
+        return (
+            <YouTube
+                ref={setPlayerRef}
+                videoId={videoId}
+                opts={playerOptions}
+                onReady={() => {
+                    onReady?.();
+                }}
+            />
+        );
+    },
+);
+
+export default WebPlayer;

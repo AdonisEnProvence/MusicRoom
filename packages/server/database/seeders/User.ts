48a42e7455bfd74d772e866c004bbdf177efffc1
Devessier <baptiste@devessier.fr>
2021-07-16 14:36:33 +0200

    chore(server): create a seeder to populate the database with two fake users

---
+++ packages/server/database/seeders/User.ts
@@ -0,0 +1,19 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+import User from 'App/Models/User';
+
+export default class UserSeeder extends BaseSeeder {
+    public static developmentOnly = true;
+
+    public async run(): Promise<void> {
+        await User.createMany([
+            {
+                uuid: 'f5ddbf01-cc01-4422-b347-67988342b558',
+                nickname: 'Web',
+            },
+            {
+                uuid: '9ed60e96-d5fc-40b3-b842-aeaa75e93972',
+                nickname: 'Mobile',
+            },
+        ]);
+    }
+}

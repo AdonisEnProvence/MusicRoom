927235aa936fe779dfd25d0d052a9648bddd9fac
Devessier <baptiste@devessier.fr>
2021-09-06 17:51:55 +0200

    feat(client): begin confirmation page

--- packages/client/screens/MusicTrackVoteCreationFormConfirmation.tsx
+++ packages/client/screens/MusicTrackVoteCreationFormConfirmation.tsx
@@ -1,3 +1,4 @@
+import { Text, View } from '@dripsy/core';
 import { useActor } from '@xstate/react';
 import React from 'react';
 import MtvRoomCreationFormScreen from '../components/MtvRoomCreationForm/MtvRoomCreationFormScreen';
@@ -9,12 +10,53 @@ const MusicTrackVoteCreationFormConfirmation: React.FC<MusicTrackVoteCreationFor
         const mtvRoomCreationActor = useCreationMtvRoomFormMachine();
         const [state, send] = useActor(mtvRoomCreationActor);
 
+        const summarySections = [
+            {
+                title: 'Name of the room',
+                value: state.context.roomName,
+            },
+
+            {
+                title: 'Opening status of the room',
+                value: state.context.isOpen ? 'Public' : 'Private',
+            },
+
+            {
+                title: 'Geolocation',
+                value: state.context.physicalConstraintPlace,
+            },
+
+            {
+                title: 'Radius',
+                value: state.context.physicalConstraintRadius,
+            },
+
+            {
+                title: 'Starts at',
+                value: state.context.physicalConstraintStartsAt,
+            },
+
+            {
+                title: 'Ends at',
+                value: state.context.physicalConstraintEndsAt,
+            },
+
+            {
+                title: 'Minimum score',
+                value: state.context.minimumVotesForATrackToBePlayed,
+            },
+        ];
+
         function handleGoBack() {
-            return undefined;
+            send({
+                type: 'GO_BACK',
+            });
         }
 
         function handleGoNext() {
-            return undefined;
+            send({
+                type: 'NEXT',
+            });
         }
 
         return (
@@ -22,7 +64,27 @@ const MusicTrackVoteCreationFormConfirmation: React.FC<MusicTrackVoteCreationFor
                 title="Confirm room creation"
                 onBackButtonPress={handleGoBack}
                 onNextButtonPress={handleGoNext}
-                Content={null}
+                Content={
+                    <>
+                        <View sx={{ marginTop: 'xl' }}>
+                            {summarySections.map(({ title, value }) => (
+                                <View
+                                    key={title}
+                                    sx={{
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                        flexWrap: 'wrap',
+                                    }}
+                                >
+                                    <Text sx={{ color: 'white' }}>{title}</Text>
+
+                                    <Text sx={{ color: 'white' }}>{value}</Text>
+                                </View>
+                            ))}
+                        </View>
+                    </>
+                }
             />
         );
     };

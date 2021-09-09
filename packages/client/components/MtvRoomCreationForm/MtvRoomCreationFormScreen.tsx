fd888dedc067a43b1f6fcbd36f50e90a878a191b
Devessier <baptiste@devessier.fr>
2021-09-09 12:09:18 +0200

    fix(client): limit max width of mtv room creation form steps

--- packages/client/components/MtvRoomCreationForm/MtvRoomCreationFormScreen.tsx
+++ packages/client/components/MtvRoomCreationForm/MtvRoomCreationFormScreen.tsx
@@ -29,76 +29,80 @@ const MtvRoomCreationFormScreen: React.FC<MtvRoomCreationFormScreenProps> = ({
     return (
         <AppScreen>
             <AppScreenContainer>
-                <View
-                    testID={testID}
-                    sx={{
-                        flex: 1,
-                        paddingTop: insets.top,
-                        paddingBottom: insets.bottom,
-                        paddingLeft: 'l',
-                        paddingRight: 'l',
+                <View testID={testID} sx={{ flexDirection: 'row', flex: 1 }}>
+                    <View
+                        sx={{
+                            flex: 1,
+                            paddingTop: insets.top,
+                            paddingBottom: insets.bottom,
+                            paddingLeft: 'l',
+                            paddingRight: 'l',
+                            maxWidth: [null, 420, 720],
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
 
-                        justifyContent: 'space-between',
-                    }}
-                >
-                    <View sx={{ marginTop: 'xl' }}>
-                        <Text
-                            sx={{
-                                paddingLeft: 'l',
-                                paddingRight: 'l',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <View sx={{ marginTop: 'xl' }}>
+                            <Text
+                                sx={{
+                                    paddingLeft: 'l',
+                                    paddingRight: 'l',
 
-                                color: 'white',
-                                fontSize: 'l',
-                                textAlign: 'center',
-                            }}
-                        >
-                            {title}
-                        </Text>
-
-                        {Content}
-                    </View>
-
-                    <View sx={{ marginBottom: 'xl', flexDirection: 'row' }}>
-                        <TouchableOpacity
-                            style={sx({
-                                flex: 1,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                borderColor: 'greyLighter',
-                                borderWidth: 1,
-                                paddingLeft: 'm',
-                                paddingRight: 'm',
-                                paddingTop: 'm',
-                                paddingBottom: 'm',
-                                borderRadius: 's',
-                                marginRight: 'l',
-                            })}
-                            onPress={onBackButtonPress}
-                        >
-                            <Text sx={{ color: 'white', fontSize: 's' }}>
-                                {backButtonText ?? 'Back'}
+                                    color: 'white',
+                                    fontSize: 'l',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                {title}
                             </Text>
-                        </TouchableOpacity>
 
-                        <TouchableOpacity
-                            style={sx({
-                                flex: 1,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                borderColor: 'greyLighter',
-                                borderWidth: 1,
-                                paddingLeft: 'm',
-                                paddingRight: 'm',
-                                paddingTop: 'm',
-                                paddingBottom: 'm',
-                                borderRadius: 's',
-                            })}
-                            onPress={onNextButtonPress}
-                        >
-                            <Text sx={{ color: 'white', fontSize: 's' }}>
-                                {nextButtonText ?? 'Next'}
-                            </Text>
-                        </TouchableOpacity>
+                            {Content}
+                        </View>
+
+                        <View sx={{ marginBottom: 'xl', flexDirection: 'row' }}>
+                            <TouchableOpacity
+                                style={sx({
+                                    flex: 1,
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    borderColor: 'greyLighter',
+                                    borderWidth: 1,
+                                    paddingLeft: 'm',
+                                    paddingRight: 'm',
+                                    paddingTop: 'm',
+                                    paddingBottom: 'm',
+                                    borderRadius: 's',
+                                    marginRight: 'l',
+                                })}
+                                onPress={onBackButtonPress}
+                            >
+                                <Text sx={{ color: 'white', fontSize: 's' }}>
+                                    {backButtonText ?? 'Back'}
+                                </Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity
+                                style={sx({
+                                    flex: 1,
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    borderColor: 'greyLighter',
+                                    borderWidth: 1,
+                                    paddingLeft: 'm',
+                                    paddingRight: 'm',
+                                    paddingTop: 'm',
+                                    paddingBottom: 'm',
+                                    borderRadius: 's',
+                                })}
+                                onPress={onNextButtonPress}
+                            >
+                                <Text sx={{ color: 'white', fontSize: 's' }}>
+                                    {nextButtonText ?? 'Next'}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
             </AppScreenContainer>

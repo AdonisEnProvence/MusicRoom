6b885d8009f0c6937175e50b16f537eb2d56c03c
Devessier <baptiste@devessier.fr>
2021-09-10 11:46:45 +0200

    build(client): fix issue with environment variable not beeing replaced

--- packages/client/constants/ApiKeys.ts
+++ packages/client/constants/ApiKeys.ts
@@ -1,5 +1,6 @@
 import * as z from 'zod';
 
+const RAW_GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY;
 export const GOOGLE_PLACES_API_KEY = z
     .string()
-    .parse(process.env.GOOGLE_PLACES_API_KEY);
+    .parse(RAW_GOOGLE_PLACES_API_KEY);

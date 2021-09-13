44547815cfded41a2094c4980aec0941ee20fc46
Devessier <baptiste@devessier.fr>
2021-09-13 10:06:47 +0200

    test(client): compile react native picker dependency

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'jest-expo',
     transformIgnorePatterns: [
-        'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|dripsy|@dripsy/.*|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+        'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|dripsy|@dripsy/.*|@react-native-community|@react-native-picker|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
     ],
     testPathIgnorePatterns: ['/node_modules/', 'dist'],
     setupFilesAfterEnv: [

39fa4275ffcb8ffc52fef1c5e4749a2041815395
Devessier <baptiste@devessier.fr>
2021-09-15 12:41:40 +0200

    feat(client): show initial tracks on confirmation screen of mtv room creation form

--- packages/client/machines/creationMtvRoomForm.ts
+++ packages/client/machines/creationMtvRoomForm.ts
@@ -1,3 +1,4 @@
+import { TrackMetadata } from '@musicroom/types';
 import {
     StateMachine,
     EventFrom,
@@ -10,6 +11,7 @@ import {
 import { createModel } from 'xstate/lib/model';
 import * as z from 'zod';
 import { navigateFromRef } from '../navigation/RootNavigation';
+import { fetchTracksByID } from '../services/search-tracks';
 import { CreationMtvRoomFormMachineToAppMusicPlayerMachineEvents } from './appMusicPlayerMachine';
 
 export type MtvRoomPlayingMode = 'BROADCAST' | 'DIRECT';
@@ -25,6 +27,9 @@ export type MtvRoomMinimumVotesForATrackToBePlayed = z.infer<
 
 const creationMtvRoomFormModel = createModel(
     {
+        initialTracksIDs: [] as string[],
+        initialTracksMetadata: undefined as TrackMetadata[] | undefined,
+
         roomName: '',
         isOpen: false,
         onlyInvitedUsersCanVote: false,
@@ -75,9 +80,16 @@ const creationMtvRoomFormModel = createModel(
             GO_BACK: () => ({}),
 
             NEXT: () => ({}),
+
+            RECEIVED_INITIAL_TRACKS_METADATA: (
+                tracksMetadata: TrackMetadata[],
+            ) => ({ tracksMetadata }),
         },
     },
 );
+
+export const creationMtvRoomFormInitialContext =
+    creationMtvRoomFormModel.initialContext;
 
 type CreationMtvRoomFormModelEventsCreators =
     typeof creationMtvRoomFormModel.events;
@@ -111,243 +123,306 @@ export type CreationMtvRoomFormDoneInvokeEvent =
     DoneInvokeEvent<CreationMtvRoomFormMachineContext>;
 
 export function createCreationMtvRoomFormMachine(): CreationMtvRoomFormMachine {
-    return creationMtvRoomFormModel.createMachine({
-        context: creationMtvRoomFormModel.initialContext,
+    return creationMtvRoomFormModel.createMachine(
+        {
+            context: creationMtvRoomFormModel.initialContext,
 
-        initial: 'roomName',
+            initial: 'roomName',
 
-        states: {
-            roomName: {
-                entry: () => {
-                    try {
-                        navigateFromRef('MusicTrackVoteCreationFormName');
-                    } catch {
-                        // An error is thrown when the modal is open.
-                        // We are not yet in MusicTrackVoteCreationForm and
-                        // we can there is no screen called MusicTrackVoteCreationFormName.
-                        // This is not a problem that the first call does not succeed
-                        // as we already perform the redirection in openCreationMtvRoomFormModal action.
-                        // It is particularly useful to handle redirection to Name step.
-                    }
+            states: {
+                roomName: {
+                    entry: () => {
+                        try {
+                            navigateFromRef('MusicTrackVoteCreationFormName');
+                        } catch {
+                            // An error is thrown when the modal is open.
+                            // We are not yet in MusicTrackVoteCreationForm and
+                            // we can there is no screen called MusicTrackVoteCreationFormName.
+                            // This is not a problem that the first call does not succeed
+                            // as we already perform the redirection in openCreationMtvRoomFormModal action.
+                            // It is particularly useful to handle redirection to Name step.
+                        }
+                    },
+
+                    on: {
+                        SET_ROOM_NAME: {
+                            actions: assignRoomName,
+                        },
+
+                        FORWARD_MODAL_CLOSER: {
+                            actions: sendParent<
+                                CreationMtvRoomFormMachineContext,
+                                ForwardModalCloserEvent,
+                                CreationMtvRoomFormMachineToAppMusicPlayerMachineEvents
+                            >((_context, event) => ({
+                                type: 'SAVE_MTV_ROOM_CREATION_MODAL_CLOSER',
+                                closeModal: event.closeModal,
+                            })),
+                        },
+
+                        GO_BACK: {
+                            actions: sendParent<
+                                CreationMtvRoomFormMachineContext,
+                                any,
+                                CreationMtvRoomFormMachineToAppMusicPlayerMachineEvents
+                            >({
+                                type: 'EXIT_MTV_ROOM_CREATION',
+                            }),
+                        },
+
+                        NEXT: {
+                            target: 'openingStatus',
+                        },
+                    },
                 },
 
-                on: {
-                    SET_ROOM_NAME: {
-                        actions: assignRoomName,
+                openingStatus: {
+                    entry: () => {
+                        navigateFromRef(
+                            'MusicTrackVoteCreationFormOpeningStatus',
+                        );
                     },
 
-                    FORWARD_MODAL_CLOSER: {
-                        actions: sendParent<
-                            CreationMtvRoomFormMachineContext,
-                            ForwardModalCloserEvent,
-                            CreationMtvRoomFormMachineToAppMusicPlayerMachineEvents
-                        >((_context, event) => ({
-                            type: 'SAVE_MTV_ROOM_CREATION_MODAL_CLOSER',
-                            closeModal: event.closeModal,
-                        })),
-                    },
+                    initial: 'public',
 
-                    GO_BACK: {
-                        actions: sendParent<
-                            CreationMtvRoomFormMachineContext,
-                            any,
-                            CreationMtvRoomFormMachineToAppMusicPlayerMachineEvents
-                        >({
-                            type: 'EXIT_MTV_ROOM_CREATION',
-                        }),
-                    },
+                    states: {
+                        public: {
+                            tags: 'isRoomPublic',
 
-                    NEXT: {
-                        target: 'openingStatus',
-                    },
-                },
-            },
+                            entry: resetOnlyInvitedUsersCanVote,
 
-            openingStatus: {
-                entry: () => {
-                    navigateFromRef('MusicTrackVoteCreationFormOpeningStatus');
-                },
-
-                initial: 'public',
-
-                states: {
-                    public: {
-                        tags: 'isRoomPublic',
-
-                        entry: resetOnlyInvitedUsersCanVote,
-
-                        on: {
-                            SET_INVITED_USERS_VOTE_RESTRICTION: {
-                                actions: creationMtvRoomFormModel.assign({
-                                    onlyInvitedUsersCanVote: (
-                                        _context,
-                                        { onlyInvitedUsersCanVote },
-                                    ) => onlyInvitedUsersCanVote,
-                                }),
+                            on: {
+                                SET_INVITED_USERS_VOTE_RESTRICTION: {
+                                    actions: creationMtvRoomFormModel.assign({
+                                        onlyInvitedUsersCanVote: (
+                                            _context,
+                                            { onlyInvitedUsersCanVote },
+                                        ) => onlyInvitedUsersCanVote,
+                                    }),
+                                },
                             },
                         },
+
+                        private: {
+                            tags: 'isRoomPrivate',
+                        },
                     },
 
-                    private: {
-                        tags: 'isRoomPrivate',
-                    },
-                },
+                    on: {
+                        SET_OPENING_STATUS: [
+                            {
+                                cond: (_context, { isOpen }) => isOpen === true,
 
-                on: {
-                    SET_OPENING_STATUS: [
-                        {
-                            cond: (_context, { isOpen }) => isOpen === true,
+                                target: '.public',
 
-                            target: '.public',
+                                actions: assignIsOpen,
+                            },
 
-                            actions: assignIsOpen,
+                            {
+                                target: '.private',
+
+                                actions: assignIsOpen,
+                            },
+                        ],
+
+                        GO_BACK: {
+                            target: 'roomName',
                         },
 
-                        {
-                            target: '.private',
-
-                            actions: assignIsOpen,
+                        NEXT: {
+                            target: 'physicalConstraints',
                         },
-                    ],
-
-                    GO_BACK: {
-                        target: 'roomName',
-                    },
-
-                    NEXT: {
-                        target: 'physicalConstraints',
-                    },
-                },
-            },
-
-            physicalConstraints: {
-                entry: () => {
-                    navigateFromRef(
-                        'MusicTrackVoteCreationFormPhysicalConstraints',
-                    );
-                },
-
-                initial: 'isNotRestricted',
-
-                states: {
-                    isRestricted: {
-                        tags: 'hasPhysicalConstraints',
-                    },
-
-                    isNotRestricted: {
-                        tags: 'hasNoPhysicalConstraints',
                     },
                 },
 
-                on: {
-                    SET_PHYSICAL_CONSTRAINTS_STATUS: [
-                        {
-                            cond: (_context, { isRestricted }) =>
-                                isRestricted === true,
+                physicalConstraints: {
+                    entry: () => {
+                        navigateFromRef(
+                            'MusicTrackVoteCreationFormPhysicalConstraints',
+                        );
+                    },
 
-                            target: '.isRestricted',
+                    initial: 'isNotRestricted',
 
-                            actions: assignHasPhysicalConstraints,
+                    states: {
+                        isRestricted: {
+                            tags: 'hasPhysicalConstraints',
                         },
 
-                        {
-                            target: '.isNotRestricted',
-
-                            actions: assignHasPhysicalConstraints,
+                        isNotRestricted: {
+                            tags: 'hasNoPhysicalConstraints',
                         },
-                    ],
-
-                    SET_PHYSICAL_CONSTRAINT_PLACE: {
-                        actions: assignPhysicalConstraintPlace,
                     },
 
-                    SET_PHYSICAL_CONSTRAINT_RADIUS: {
-                        actions: assignPhysicalConstraintRadius,
-                    },
+                    on: {
+                        SET_PHYSICAL_CONSTRAINTS_STATUS: [
+                            {
+                                cond: (_context, { isRestricted }) =>
+                                    isRestricted === true,
 
-                    SET_PHYSICAL_CONSTRAINT_STARTS_AT: {
-                        actions: assignPhysicalConstraintStartsAt,
-                    },
+                                target: '.isRestricted',
 
-                    SET_PHYSICAL_CONSTRAINT_ENDS_AT: {
-                        actions: assignPhysicalConstraintEndsAt,
-                    },
+                                actions: assignHasPhysicalConstraints,
+                            },
 
-                    GO_BACK: {
-                        target: 'openingStatus',
-                    },
+                            {
+                                target: '.isNotRestricted',
 
-                    NEXT: {
-                        target: 'playingMode',
-                    },
-                },
-            },
+                                actions: assignHasPhysicalConstraints,
+                            },
+                        ],
 
-            playingMode: {
-                entry: () => {
-                    navigateFromRef('MusicTrackVoteCreationFormPlayingMode');
-                },
+                        SET_PHYSICAL_CONSTRAINT_PLACE: {
+                            actions: assignPhysicalConstraintPlace,
+                        },
 
-                on: {
-                    SET_PLAYING_MODE: {
-                        actions: assignPlayingMode,
-                    },
+                        SET_PHYSICAL_CONSTRAINT_RADIUS: {
+                            actions: assignPhysicalConstraintRadius,
+                        },
 
-                    GO_BACK: {
-                        target: 'physicalConstraints',
-                    },
+                        SET_PHYSICAL_CONSTRAINT_STARTS_AT: {
+                            actions: assignPhysicalConstraintStartsAt,
+                        },
 
-                    NEXT: {
-                        target: 'votesConstraints',
-                    },
-                },
-            },
+                        SET_PHYSICAL_CONSTRAINT_ENDS_AT: {
+                            actions: assignPhysicalConstraintEndsAt,
+                        },
 
-            votesConstraints: {
-                entry: () => {
-                    navigateFromRef(
-                        'MusicTrackVoteCreationFormVotesConstraints',
-                    );
-                },
+                        GO_BACK: {
+                            target: 'openingStatus',
+                        },
 
-                on: {
-                    SET_MINIMUM_VOTES_FOR_A_TRACK_TO_BE_PLAYED: {
-                        actions: assignMinimumVotesForATrackToBePlayed,
-                    },
-
-                    GO_BACK: {
-                        target: 'playingMode',
-                    },
-
-                    NEXT: {
-                        target: 'waitingForConfirmation',
+                        NEXT: {
+                            target: 'playingMode',
+                        },
                     },
                 },
-            },
 
-            waitingForConfirmation: {
-                entry: () => {
-                    navigateFromRef('MusicTrackVoteCreationFormConfirmation');
-                },
-
-                on: {
-                    GO_BACK: {
-                        target: 'votesConstraints',
+                playingMode: {
+                    entry: () => {
+                        navigateFromRef(
+                            'MusicTrackVoteCreationFormPlayingMode',
+                        );
                     },
 
-                    NEXT: {
-                        target: 'confirmed',
+                    on: {
+                        SET_PLAYING_MODE: {
+                            actions: assignPlayingMode,
+                        },
+
+                        GO_BACK: {
+                            target: 'physicalConstraints',
+                        },
+
+                        NEXT: {
+                            target: 'votesConstraints',
+                        },
                     },
                 },
-            },
 
-            confirmed: {
-                type: 'final',
+                votesConstraints: {
+                    entry: () => {
+                        navigateFromRef(
+                            'MusicTrackVoteCreationFormVotesConstraints',
+                        );
+                    },
 
-                data: (context) => context,
+                    on: {
+                        SET_MINIMUM_VOTES_FOR_A_TRACK_TO_BE_PLAYED: {
+                            actions: assignMinimumVotesForATrackToBePlayed,
+                        },
+
+                        GO_BACK: {
+                            target: 'playingMode',
+                        },
+
+                        NEXT: {
+                            target: 'waitingForConfirmation',
+                        },
+                    },
+                },
+
+                waitingForConfirmation: {
+                    entry: () => {
+                        navigateFromRef(
+                            'MusicTrackVoteCreationFormConfirmation',
+                        );
+                    },
+
+                    initial: 'fetchingInitialTracksInformation',
+
+                    states: {
+                        fetchingInitialTracksInformation: {
+                            invoke: {
+                                src: 'fetchTracksInformation',
+                            },
+
+                            on: {
+                                RECEIVED_INITIAL_TRACKS_METADATA: {
+                                    target: 'debouncing',
+
+                                    actions: creationMtvRoomFormModel.assign({
+                                        initialTracksMetadata: (_, event) =>
+                                            event.tracksMetadata,
+                                    }),
+                                },
+                            },
+                        },
+
+                        debouncing: {
+                            after: {
+                                1000: {
+                                    target: 'fetchedInitialTracksInformation',
+                                },
+                            },
+                        },
+
+                        fetchedInitialTracksInformation: {
+                            tags: 'hasFetchedInitialTracksInformation',
+                        },
+                    },
+
+                    on: {
+                        GO_BACK: {
+                            target: 'votesConstraints',
+                        },
+
+                        NEXT: {
+                            target: 'confirmed',
+                        },
+                    },
+                },
+
+                confirmed: {
+                    type: 'final',
+
+                    data: (context) => context,
+                },
             },
         },
-    });
+
+        {
+            services: {
+                fetchTracksInformation: (context) => async (sendBack) => {
+                    try {
+                        const tracksMetadata = await fetchTracksByID(
+                            context.initialTracksIDs,
+                        );
+                        if (tracksMetadata === undefined) {
+                            throw new Error('Could not fetch tracks metadata');
+                        }
+
+                        sendBack({
+                            type: 'RECEIVED_INITIAL_TRACKS_METADATA',
+                            tracksMetadata,
+                        });
+                    } catch (err) {
+                        console.error(err);
+                    }
+                },
+            },
+        },
+    );
 }
 
 const assignRoomName = creationMtvRoomFormModel.assign(

629a10e50440857e6774d2ef587143b38ae5255d
Devessier <baptiste@devessier.fr>
2021-09-16 12:18:38 +0200

    feat(client): validate room name text field

--- packages/client/screens/MusicTrackVoteCreationFormName.tsx
+++ packages/client/screens/MusicTrackVoteCreationFormName.tsx
@@ -2,11 +2,16 @@ import { useActor } from '@xstate/react';
 import { Text, View } from 'dripsy';
 import React from 'react';
 import { useEffect } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { TextField } from '../components/kit';
 import MtvRoomCreationFormScreen from '../components/MtvRoomCreationForm/MtvRoomCreationFormScreen';
 import { useCreationMtvRoomFormMachine } from '../contexts/MusicPlayerContext';
 import { CreationMtvRoomFormActorRef } from '../machines/creationMtvRoomForm';
 import { MusicTrackVoteCreationFormNameScreenProps } from '../types';
+
+interface FormFieldValues {
+    roomName: string;
+}
 
 const MusicTrackVoteCreationFormName: React.FC<
     MusicTrackVoteCreationFormNameScreenProps & {
@@ -14,7 +19,12 @@ const MusicTrackVoteCreationFormName: React.FC<
     }
 > = ({ navigation, mtvRoomCreationActor }) => {
     const [state, send] = useActor(mtvRoomCreationActor);
-    const currentRoomName = state.context.roomName;
+    const defaultRoomName = state.context.roomName;
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormFieldValues>();
 
     useEffect(() => {
         function closeModal() {
@@ -51,16 +61,38 @@ const MusicTrackVoteCreationFormName: React.FC<
         <MtvRoomCreationFormScreen
             title="What is the name of the room?"
             onBackButtonPress={handleGoBack}
-            onNextButtonPress={handleGoNext}
+            onNextButtonPress={handleSubmit(({ roomName }) => {
+                handleRoomNameChange(roomName);
+
+                handleGoNext();
+            })}
             Content={
                 <>
                     <View sx={{ marginTop: 'xl' }}>
-                        <TextField
-                            value={currentRoomName}
-                            placeholder="Francis Cabrel OnlyFans"
-                            onChangeText={handleRoomNameChange}
-                            placeholderTextColor="#fff"
+                        <Controller
+                            control={control}
+                            rules={{
+                                required: true,
+                            }}
+                            render={({
+                                field: { onChange, onBlur, value },
+                            }) => (
+                                <TextField
+                                    value={value}
+                                    onBlur={onBlur}
+                                    onChangeText={onChange}
+                                    placeholder="Francis Cabrel OnlyFans"
+                                    placeholderTextColor="#fff"
+                                />
+                            )}
+                            name="roomName"
+                            defaultValue={defaultRoomName}
                         />
+                        {errors.roomName && (
+                            <Text sx={{ color: 'red', marginTop: 's' }}>
+                                A room name must be set.
+                            </Text>
+                        )}
                     </View>
 
                     <View

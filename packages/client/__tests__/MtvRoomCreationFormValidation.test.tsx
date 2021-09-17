976f7e1045a973d31d188cfcb8bf71dcc5792b06
Devessier <baptiste@devessier.fr>
2021-09-17 20:01:08 +0200

    test(client): test room name validation with @xstate/test

---
+++ packages/client/__tests__/MtvRoomCreationFormValidation.test.tsx
@@ -0,0 +1,186 @@
+import React from 'react';
+import { createModel } from 'xstate/lib/model';
+import { createModel as createTestModel } from '@xstate/test';
+import * as z from 'zod';
+import { fireEvent, noop, render, waitFor } from '../tests/tests-utils';
+import {
+    MusicTrackVoteCreationFormNameContent,
+    MusicTrackVoteCreationFormNameFormFieldValues,
+} from '../screens/MusicTrackVoteCreationFormName';
+
+interface TestingContext {
+    triggerSubmit: () => void;
+    submitSpy: jest.Mock<void, [MusicTrackVoteCreationFormNameFormFieldValues]>;
+
+    screen: ReturnType<typeof render>;
+}
+
+const roomNameValidationModel = createModel(
+    {},
+    {
+        events: {
+            TYPE_ROOM_NAME_AND_SUBMIT: (roomName: string) => ({ roomName }),
+
+            SUBMIT: () => ({}),
+        },
+    },
+);
+
+const roomNameValidationMachine = roomNameValidationModel.createMachine({
+    id: 'roomNameValidation',
+
+    initial: 'idle',
+
+    states: {
+        idle: {
+            meta: {
+                test: ({ screen }: TestingContext) => {
+                    const roomNameInput =
+                        screen.getByPlaceholderText(/francis.*cabrel/i);
+                    expect(roomNameInput).toBeTruthy();
+                },
+            },
+
+            on: {
+                SUBMIT: {
+                    target: 'inputErrors.fieldRequired',
+                },
+
+                TYPE_ROOM_NAME_AND_SUBMIT: [
+                    {
+                        cond: (_, { roomName }) => {
+                            const isEmpty = roomName === '';
+
+                            return isEmpty;
+                        },
+
+                        target: 'inputErrors.fieldRequired',
+                    },
+
+                    {
+                        target: 'success',
+                    },
+                ],
+            },
+        },
+
+        inputErrors: {
+            initial: 'fieldRequired',
+
+            states: {
+                fieldRequired: {
+                    meta: {
+                        test: async ({ screen, submitSpy }: TestingContext) => {
+                            const requiredError = await screen.findByRole(
+                                'alert',
+                            );
+                            expect(requiredError).toBeTruthy();
+                            expect(requiredError).toHaveTextContent(
+                                /room.*name.*must.*be.*set/i,
+                            );
+
+                            expect(submitSpy).not.toHaveBeenCalled();
+                        },
+                    },
+                },
+            },
+        },
+
+        success: {
+            type: 'final',
+
+            meta: {
+                test: async ({ screen, submitSpy }: TestingContext) => {
+                    await waitFor(() =>
+                        expect(screen.queryAllByRole('alert')).toHaveLength(0),
+                    );
+
+                    expect(submitSpy).toHaveBeenCalled();
+                },
+            },
+        },
+    },
+});
+
+const TypeRoomNameAndSubmitEvent = z
+    .object({
+        roomName: z.string(),
+    })
+    .nonstrict();
+type TypeRoomNameAndSubmitEvent = z.infer<typeof TypeRoomNameAndSubmitEvent>;
+
+const roomNameValidationTestModel = createTestModel<TestingContext>(
+    roomNameValidationMachine,
+).withEvents({
+    TYPE_ROOM_NAME_AND_SUBMIT: {
+        exec: ({ screen, triggerSubmit }, event) => {
+            const { roomName } = TypeRoomNameAndSubmitEvent.parse(event);
+
+            const roomNameInput =
+                screen.getByPlaceholderText(/francis.*cabrel/i);
+
+            fireEvent.changeText(roomNameInput, roomName);
+
+            triggerSubmit();
+        },
+
+        cases: [
+            {
+                roomName: '',
+            } as TypeRoomNameAndSubmitEvent,
+
+            {
+                roomName: 'Biolay Fans',
+            } as TypeRoomNameAndSubmitEvent,
+        ],
+    },
+
+    SUBMIT: ({ triggerSubmit }) => {
+        triggerSubmit();
+    },
+});
+
+describe('MtvRoomCreationForm validation', () => {
+    const testPlans = roomNameValidationTestModel.getSimplePathPlans({
+        filter: (state) => state.matches('idle') === false,
+    });
+
+    testPlans.forEach((plan) => {
+        describe(plan.description, () => {
+            plan.paths.forEach((path) => {
+                it(path.description, async () => {
+                    const submitSpy: jest.Mock<
+                        void,
+                        [MusicTrackVoteCreationFormNameFormFieldValues]
+                    > = jest.fn();
+
+                    const screen = render(
+                        <MusicTrackVoteCreationFormNameContent
+                            handleGoBack={noop}
+                            handleGoNext={submitSpy}
+                            defaultRoomName=""
+                        />,
+                    );
+
+                    function triggerSubmit() {
+                        const submitButton = screen.getByText(/next/i);
+
+                        fireEvent.press(submitButton);
+                    }
+
+                    await path.test({
+                        triggerSubmit,
+                        submitSpy,
+                        screen,
+                    });
+                });
+            });
+        });
+    });
+
+    it('should have full coverage', () => {
+        roomNameValidationTestModel.testCoverage({
+            filter: (stateNode) => !!stateNode.meta,
+        });
+    });
+});
--- packages/client/screens/MusicTrackVoteCreationFormName.tsx
+++ packages/client/screens/MusicTrackVoteCreationFormName.tsx
@@ -2,16 +2,84 @@ import { useActor } from '@xstate/react';
 import { Text, View } from 'dripsy';
 import React from 'react';
 import { useEffect } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { TextField } from '../components/kit';
 import MtvRoomCreationFormScreen from '../components/MtvRoomCreationForm/MtvRoomCreationFormScreen';
 import { useCreationMtvRoomFormMachine } from '../contexts/MusicPlayerContext';
 import { CreationMtvRoomFormActorRef } from '../machines/creationMtvRoomForm';
 import { MusicTrackVoteCreationFormNameScreenProps } from '../types';
 
-interface FormFieldValues {
+export interface MusicTrackVoteCreationFormNameFormFieldValues {
     roomName: string;
 }
+
+interface MusicTrackVoteCreationFormNameContentProps {
+    defaultRoomName: string;
+    handleGoBack: () => void;
+    handleGoNext: SubmitHandler<MusicTrackVoteCreationFormNameFormFieldValues>;
+}
+
+export const MusicTrackVoteCreationFormNameContent: React.FC<MusicTrackVoteCreationFormNameContentProps> =
+    ({ defaultRoomName, handleGoBack, handleGoNext }) => {
+        const {
+            control,
+            handleSubmit,
+            formState: { errors },
+        } = useForm<MusicTrackVoteCreationFormNameFormFieldValues>();
+
+        return (
+            <MtvRoomCreationFormScreen
+                title="What is the name of the room?"
+                onBackButtonPress={handleGoBack}
+                onNextButtonPress={handleSubmit(handleGoNext)}
+                Content={
+                    <>
+                        <View sx={{ marginTop: 'xl' }}>
+                            <Controller
+                                control={control}
+                                rules={{
+                                    required: true,
+                                }}
+                                render={({
+                                    field: { onChange, onBlur, value },
+                                }) => (
+                                    <TextField
+                                        value={value}
+                                        onBlur={onBlur}
+                                        onChangeText={onChange}
+                                        placeholder="Francis Cabrel OnlyFans"
+                                        placeholderTextColor="#fff"
+                                    />
+                                )}
+                                name="roomName"
+                                defaultValue={defaultRoomName}
+                            />
+                            {errors.roomName && (
+                                <Text
+                                    accessibilityRole="alert"
+                                    sx={{ color: 'red', marginTop: 's' }}
+                                >
+                                    A room name must be set.
+                                </Text>
+                            )}
+                        </View>
+
+                        <View
+                            sx={{
+                                marginTop: 'xl',
+                                backgroundColor: 'greyLighter',
+                                color: 'greyLight',
+                                padding: 'm',
+                                borderRadius: 's',
+                            }}
+                        >
+                            <Text>This is an advice</Text>
+                        </View>
+                    </>
+                }
+            />
+        );
+    };
 
 const MusicTrackVoteCreationFormName: React.FC<
     MusicTrackVoteCreationFormNameScreenProps & {
@@ -20,11 +88,6 @@ const MusicTrackVoteCreationFormName: React.FC<
 > = ({ navigation, mtvRoomCreationActor }) => {
     const [state, send] = useActor(mtvRoomCreationActor);
     const defaultRoomName = state.context.roomName;
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FormFieldValues>();
 
     useEffect(() => {
         function closeModal() {
@@ -58,56 +121,14 @@ const MusicTrackVoteCreationFormName: React.FC<
     }
 
     return (
-        <MtvRoomCreationFormScreen
-            title="What is the name of the room?"
-            onBackButtonPress={handleGoBack}
-            onNextButtonPress={handleSubmit(({ roomName }) => {
+        <MusicTrackVoteCreationFormNameContent
+            defaultRoomName={defaultRoomName}
+            handleGoBack={handleGoBack}
+            handleGoNext={({ roomName }) => {
                 handleRoomNameChange(roomName);
 
                 handleGoNext();
-            })}
-            Content={
-                <>
-                    <View sx={{ marginTop: 'xl' }}>
-                        <Controller
-                            control={control}
-                            rules={{
-                                required: true,
-                            }}
-                            render={({
-                                field: { onChange, onBlur, value },
-                            }) => (
-                                <TextField
-                                    value={value}
-                                    onBlur={onBlur}
-                                    onChangeText={onChange}
-                                    placeholder="Francis Cabrel OnlyFans"
-                                    placeholderTextColor="#fff"
-                                />
-                            )}
-                            name="roomName"
-                            defaultValue={defaultRoomName}
-                        />
-                        {errors.roomName && (
-                            <Text sx={{ color: 'red', marginTop: 's' }}>
-                                A room name must be set.
-                            </Text>
-                        )}
-                    </View>
-
-                    <View
-                        sx={{
-                            marginTop: 'xl',
-                            backgroundColor: 'greyLighter',
-                            color: 'greyLight',
-                            padding: 'm',
-                            borderRadius: 's',
-                        }}
-                    >
-                        <Text>This is an advice</Text>
-                    </View>
-                </>
-            }
+            }}
         />
     );
 };

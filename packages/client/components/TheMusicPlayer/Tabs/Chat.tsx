9c8c6362ca567b90795673e016fb60cd6292e50b
Devessier <baptiste@devessier.fr>
2021-10-07 12:53:39 +0200

    feat(client): create text input and send button of the chat

--- packages/client/components/TheMusicPlayer/Tabs/Chat.tsx
+++ packages/client/components/TheMusicPlayer/Tabs/Chat.tsx
@@ -1,10 +1,47 @@
-import { Text, View } from 'dripsy';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { View, TextInput, useSx } from 'dripsy';
+import { Ionicons } from '@expo/vector-icons';
 
 const ChatTab: React.FC = () => {
+    const sx = useSx();
+
     return (
-        <View>
-            <Text sx={{ color: 'white' }}>Chat</Text>
+        <View sx={{ flex: 1 }}>
+            <View sx={{ backgroundColor: 'secondary', flex: 1 }} />
+
+            <View
+                sx={{
+                    flexShrink: 1,
+                    paddingTop: 'm',
+                    paddingBottom: 'l',
+
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                }}
+            >
+                <TextInput
+                    placeholder="Write a message..."
+                    // FIXME: From Colors.tsx file
+                    placeholderTextColor="rgb(149, 150, 156)"
+                    sx={{
+                        flex: 1,
+                        padding: 'm',
+                        marginRight: 'm',
+                        backgroundColor: 'greyLight',
+                        color: 'white',
+                        borderRadius: 's',
+                    }}
+                />
+
+                <TouchableOpacity
+                    style={sx({
+                        padding: 's',
+                    })}
+                >
+                    <Ionicons name="send" size={20} color="white" />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };

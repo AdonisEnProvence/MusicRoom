0921db43b27bb12116d5d29a6590b977af4de8c5
Devessier <baptiste@devessier.fr>
2021-10-07 14:12:01 +0200

    feat(client): create messages list

--- packages/client/components/TheMusicPlayer/Tabs/Chat.tsx
+++ packages/client/components/TheMusicPlayer/Tabs/Chat.tsx
@@ -1,20 +1,134 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { View, TextInput, useSx } from 'dripsy';
+import React, { useState } from 'react';
+import { TouchableOpacity, FlatList } from 'react-native';
+import { View, TextInput, useSx, Text } from 'dripsy';
 import { Ionicons } from '@expo/vector-icons';
+import { datatype, internet, lorem } from 'faker';
 
 const ChatTab: React.FC = () => {
     const sx = useSx();
+    const [messages, setMessages] = useState<
+        {
+            content: string;
+            creatorName: string;
+            isMyMessage: boolean;
+        }[]
+    >([
+        {
+            content: `1 ${lorem.sentences()}`,
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+        {
+            content: lorem.sentences(),
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+        {
+            content: lorem.sentences(),
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+        {
+            content: lorem.sentences(),
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+        {
+            content: lorem.sentences(),
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+        {
+            content: lorem.sentences(),
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+        {
+            content: lorem.sentences(),
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+        {
+            content: lorem.sentences(),
+            creatorName: internet.userName(),
+            isMyMessage: datatype.boolean(),
+        },
+    ]);
+    const [message, setMessage] = useState('');
+
+    function handleMessageSubmit() {
+        setMessages((messages) => [
+            {
+                content: message,
+                creatorName: internet.userName(),
+                isMyMessage: true,
+            },
+            ...messages,
+        ]);
+
+        setMessage('');
+    }
 
     return (
         <View sx={{ flex: 1 }}>
             <View
                 sx={{
-                    backgroundColor: 'secondary',
-                    flex: 1,
-                    flexBasis: [undefined, 400],
+                    flexGrow: [1, undefined],
+                    height: [undefined, 400],
                 }}
-            />
+            >
+                <FlatList
+                    data={messages}
+                    inverted
+                    renderItem={({
+                        item: { content, creatorName, isMyMessage },
+                        index,
+                    }) => {
+                        const isFirstItem = index === 0;
+                        const isNotFirstItem = !isFirstItem;
+
+                        return (
+                            <View
+                                sx={{
+                                    alignItems: isMyMessage
+                                        ? 'flex-end'
+                                        : 'flex-start',
+                                    marginBottom: isNotFirstItem
+                                        ? 'l'
+                                        : undefined,
+                                }}
+                            >
+                                <Text
+                                    sx={{
+                                        color: 'greyLighter',
+                                        marginBottom: 's',
+                                        fontSize: 'xxs',
+                                    }}
+                                >
+                                    {creatorName}
+                                </Text>
+
+                                <View
+                                    sx={{
+                                        borderRadius: 's',
+                                        padding: 'm',
+                                        backgroundColor: 'greyLight',
+                                        maxWidth: '80%',
+                                    }}
+                                >
+                                    <Text sx={{ color: 'white' }}>
+                                        {content}
+                                    </Text>
+                                </View>
+                            </View>
+                        );
+                    }}
+                    keyExtractor={({ content }) => content}
+                    style={sx({
+                        flex: 1,
+                    })}
+                />
+            </View>
 
             <View
                 sx={{
@@ -27,6 +141,9 @@ const ChatTab: React.FC = () => {
                 }}
             >
                 <TextInput
+                    value={message}
+                    onChangeText={setMessage}
+                    onSubmitEditing={handleMessageSubmit}
                     placeholder="Write a message..."
                     // FIXME: From Colors.tsx file
                     placeholderTextColor="rgb(149, 150, 156)"
@@ -44,6 +161,7 @@ const ChatTab: React.FC = () => {
                     style={sx({
                         padding: 's',
                     })}
+                    onPress={handleMessageSubmit}
                 >
                     <Ionicons name="send" size={20} color="white" />
                 </TouchableOpacity>

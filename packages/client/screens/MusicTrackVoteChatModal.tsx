bca4fb9a3b360503c53e21dfdf0b0b6c61f5d104
Devessier <baptiste@devessier.fr>
2021-10-12 11:58:37 +0200

    feat(client): prevent keyboard from overlapping with the input

--- packages/client/screens/MusicTrackVoteChatModal.tsx
+++ packages/client/screens/MusicTrackVoteChatModal.tsx
@@ -7,7 +7,14 @@ import {
 } from '../components/kit';
 import { MusicTrackVoteChatModalProps } from '../types';
 import { useMusicPlayer } from '../contexts/MusicPlayerContext';
-import { FlatList, TouchableOpacity } from 'react-native';
+import {
+    FlatList,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { MtvRoomChatMessage, normalizeChatMessage } from '@musicroom/types';
 import { useMachine } from '@xstate/react';
@@ -102,106 +109,122 @@ const ChatView: React.FC<ChatViewProps> = ({
             />
 
             <AppScreenContainer>
-                <View sx={{ flex: 1, paddingBottom: insets.bottom }}>
-                    <View
-                        sx={{
-                            flex: 1,
-                        }}
-                    >
-                        <FlatList
-                            data={messages}
-                            inverted
-                            renderItem={({
-                                item: { text, authorID, authorName },
-                                index,
-                            }) => {
-                                const isFirstItem = index === 0;
-                                const isNotFirstItem = !isFirstItem;
-                                const isMyMessage = authorID === currentUserID;
-                                const isNotMyMessage = !isMyMessage;
-
-                                return (
-                                    <View
-                                        sx={{
-                                            alignItems: isMyMessage
-                                                ? 'flex-end'
-                                                : 'flex-start',
-                                            marginBottom: isNotFirstItem
-                                                ? 'l'
-                                                : undefined,
-                                        }}
-                                    >
-                                        {isNotMyMessage && (
-                                            <Text
-                                                sx={{
-                                                    color: 'greyLighter',
-                                                    marginBottom: 's',
-                                                    fontSize: 'xxs',
-                                                }}
-                                            >
-                                                {authorName}
-                                            </Text>
-                                        )}
-
-                                        <View
-                                            sx={{
-                                                borderRadius: 's',
-                                                padding: 'm',
-                                                backgroundColor: 'greyLight',
-                                                maxWidth: '80%',
-                                            }}
-                                        >
-                                            <Text sx={{ color: 'white' }}>
-                                                {text}
-                                            </Text>
-                                        </View>
-                                    </View>
-                                );
-                            }}
-                            style={sx({
-                                flex: 1,
-                            })}
-                        />
-                    </View>
-
-                    <View
-                        sx={{
-                            flexShrink: 0,
-                            paddingTop: 'm',
-                            paddingBottom: 'l',
-
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <TextInput
-                            value={currentMessage}
-                            onChangeText={setCurrentMessage}
-                            onSubmitEditing={onSubmitEditing}
-                            placeholder="Write a message..."
-                            // FIXME: From Colors.tsx file
-                            placeholderTextColor="rgb(149, 150, 156)"
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    // The height of the screen header.
+                    // Not accurate on Android.
+                    keyboardVerticalOffset={100}
+                    style={{ flex: 1 }}
+                >
+                    <View sx={{ flex: 1, paddingBottom: insets.bottom }}>
+                        <View
                             sx={{
                                 flex: 1,
-                                padding: 'm',
-                                marginRight: 'm',
-                                backgroundColor: 'greyLight',
-                                color: 'white',
-                                borderRadius: 's',
                             }}
-                        />
-
-                        <TouchableOpacity
-                            style={sx({
-                                padding: 's',
-                            })}
-                            accessibilityLabel="Send message"
-                            onPress={onSubmitEditing}
                         >
-                            <Ionicons name="send" size={20} color="white" />
-                        </TouchableOpacity>
+                            <FlatList
+                                data={messages}
+                                inverted
+                                renderItem={({
+                                    item: { text, authorID, authorName },
+                                    index,
+                                }) => {
+                                    const isFirstItem = index === 0;
+                                    const isNotFirstItem = !isFirstItem;
+                                    const isMyMessage =
+                                        authorID === currentUserID;
+                                    const isNotMyMessage = !isMyMessage;
+
+                                    return (
+                                        <TouchableWithoutFeedback
+                                            onPress={Keyboard.dismiss}
+                                        >
+                                            <View
+                                                sx={{
+                                                    alignItems: isMyMessage
+                                                        ? 'flex-end'
+                                                        : 'flex-start',
+                                                    marginBottom: isNotFirstItem
+                                                        ? 'l'
+                                                        : undefined,
+                                                }}
+                                            >
+                                                {isNotMyMessage && (
+                                                    <Text
+                                                        sx={{
+                                                            color: 'greyLighter',
+                                                            marginBottom: 's',
+                                                            fontSize: 'xxs',
+                                                        }}
+                                                    >
+                                                        {authorName}
+                                                    </Text>
+                                                )}
+
+                                                <View
+                                                    sx={{
+                                                        borderRadius: 's',
+                                                        padding: 'm',
+                                                        backgroundColor:
+                                                            'greyLight',
+                                                        maxWidth: '80%',
+                                                    }}
+                                                >
+                                                    <Text
+                                                        sx={{ color: 'white' }}
+                                                    >
+                                                        {text}
+                                                    </Text>
+                                                </View>
+                                            </View>
+                                        </TouchableWithoutFeedback>
+                                    );
+                                }}
+                                style={sx({
+                                    flex: 1,
+                                })}
+                            />
+                        </View>
+
+                        <View
+                            sx={{
+                                flexShrink: 0,
+                                paddingTop: 'm',
+                                paddingBottom: 'l',
+
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <TextInput
+                                value={currentMessage}
+                                onChangeText={setCurrentMessage}
+                                onSubmitEditing={onSubmitEditing}
+                                placeholder="Write a message..."
+                                // FIXME: From Colors.tsx file
+                                placeholderTextColor="rgb(149, 150, 156)"
+                                sx={{
+                                    flex: 1,
+                                    padding: 'm',
+                                    marginRight: 'm',
+                                    backgroundColor: 'greyLight',
+                                    color: 'white',
+                                    borderRadius: 's',
+                                }}
+                            />
+
+                            <TouchableOpacity
+                                style={sx({
+                                    padding: 's',
+                                })}
+                                accessibilityLabel="Send message"
+                                onPress={onSubmitEditing}
+                            >
+                                <Ionicons name="send" size={20} color="white" />
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                </View>
+                </KeyboardAvoidingView>
             </AppScreenContainer>
         </AppScreen>
     );

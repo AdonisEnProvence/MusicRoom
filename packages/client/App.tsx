7c64f31e3c0ff2f244030d18c124d9038a8605c7
prastoin <tkesray@gmail.com>
2021-10-15 10:40:44 +0200

    refactor(client): remove old contexts Provider

--- packages/client/App.tsx
+++ packages/client/App.tsx
@@ -4,9 +4,8 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { ImageBackground } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { MusicPlayerContextProvider } from './contexts/MusicPlayerContext';
+import { AppContextProvider } from './contexts/AppContext';
 import { SocketContextProvider } from './contexts/SocketContext';
-import { UserContextProvider } from './contexts/UserContext';
 import useCachedResources from './hooks/useCachedResources';
 import { useTheme } from './hooks/useTheme';
 import Navigation from './navigation';
@@ -27,57 +26,55 @@ const App: React.FC = () => {
                 <SafeAreaProvider>
                     <BottomSheetModalProvider>
                         <SocketContextProvider>
-                            <UserContextProvider>
-                                <MusicPlayerContextProvider
-                                    setDisplayModal={setDisplayModal}
-                                >
-                                    {displayModal && (
-                                        <View
-                                            sx={{
-                                                zIndex: '2',
-                                                position: 'absolute',
-                                                transform: `translate(-50%,-50%)`,
-                                                left: '50%',
-                                                top: '50%',
-                                                width: '40%',
-                                                height: '40%',
-                                                backgroundColor: 'red',
+                            <AppContextProvider
+                                setDisplayModal={setDisplayModal}
+                            >
+                                {displayModal && (
+                                    <View
+                                        sx={{
+                                            zIndex: '2',
+                                            position: 'absolute',
+                                            transform: `translate(-50%,-50%)`,
+                                            left: '50%',
+                                            top: '50%',
+                                            width: '40%',
+                                            height: '40%',
+                                            backgroundColor: 'red',
+                                        }}
+                                    >
+                                        <ImageBackground
+                                            source={{
+                                                uri: `https://media-exp1.licdn.com/dms/image/C4D03AQE_UVoK5h2u8w/profile-displayphoto-shrink_200_200/0/1585838395278?e=1632960000&v=beta&t=hPLhVUJU0fzc-y5iFafdhhGrlwhSOJtAQjsCSlSLg7M`,
+                                            }}
+                                            resizeMode="cover"
+                                            style={{
+                                                width: '100%',
+                                                height: '100%',
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
                                             }}
                                         >
-                                            <ImageBackground
-                                                source={{
-                                                    uri: `https://media-exp1.licdn.com/dms/image/C4D03AQE_UVoK5h2u8w/profile-displayphoto-shrink_200_200/0/1585838395278?e=1632960000&v=beta&t=hPLhVUJU0fzc-y5iFafdhhGrlwhSOJtAQjsCSlSLg7M`,
-                                                }}
-                                                resizeMode="cover"
-                                                style={{
-                                                    width: '100%',
-                                                    height: '100%',
-                                                    display: 'flex',
-                                                    justifyContent: 'center',
-                                                    alignItems: 'center',
-                                                }}
-                                            >
-                                                <Text>Click</Text>
-                                            </ImageBackground>
-                                        </View>
-                                    )}
-                                    <Navigation
-                                        colorScheme={colorScheme}
-                                        toggleColorScheme={toggleColorScheme}
-                                        sx={{
-                                            backgroundColor: 'headerBackground',
-                                            flex: 1,
-                                        }}
-                                    />
-                                    <StatusBar
-                                        style={
-                                            colorScheme === 'dark'
-                                                ? 'light'
-                                                : 'dark'
-                                        }
-                                    />
-                                </MusicPlayerContextProvider>
-                            </UserContextProvider>
+                                            <Text>Click</Text>
+                                        </ImageBackground>
+                                    </View>
+                                )}
+                                <Navigation
+                                    colorScheme={colorScheme}
+                                    toggleColorScheme={toggleColorScheme}
+                                    sx={{
+                                        backgroundColor: 'headerBackground',
+                                        flex: 1,
+                                    }}
+                                />
+                                <StatusBar
+                                    style={
+                                        colorScheme === 'dark'
+                                            ? 'light'
+                                            : 'dark'
+                                    }
+                                />
+                            </AppContextProvider>
                         </SocketContextProvider>
                     </BottomSheetModalProvider>
                 </SafeAreaProvider>

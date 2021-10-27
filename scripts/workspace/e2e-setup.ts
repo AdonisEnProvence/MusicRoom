a31bb6553b89921437a09e13894608ed8c02999f
Devessier <baptiste@devessier.fr>
2021-10-27 12:44:21 +0200

    build: begin creating a script to launch all services for e2e testing

---
+++ scripts/workspace/e2e-setup.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env tsm
+import { $, cd, sleep, nothrow, ProcessPromise, ProcessOutput, fs } from 'zx';
+
+let temporalService: ProcessPromise<ProcessOutput>;
+let temporalWorker: ProcessPromise<ProcessOutput>;
+let temporalApi: ProcessPromise<ProcessOutput>;
+
+async function startServices() {
+    cd('packages/temporal/docker-compose');
+
+    temporalService = nothrow(
+        $`docker-compose up`.pipe(fs.createWriteStream('/dev/null')),
+    );
+
+    // Wait 10 seconds for Temporal to launch
+    await Promise.all([sleep(10_000), $`yarn worker:build`, $`yarn api:build`]);
+
+    temporalWorker = nothrow($`yarn worker:launch`);
+    temporalApi = nothrow($`yarn api:launch`);
+}
+
+async function stopServices() {
+    await temporalService.kill();
+    await temporalWorker.kill();
+    await temporalApi.kill();
+}
+
+async function runE2eTests() {
+    await sleep(10_000);
+
+    await $`echo run tests`;
+}
+
+async function script() {
+    await startServices();
+
+    await runE2eTests();
+
+    await stopServices();
+}
+
+void script();

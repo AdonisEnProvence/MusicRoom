0111149bf73f147300323121c864044c98b2d7b1
Devessier <baptiste@devessier.fr>
2021-10-28 13:41:55 +0200

    test(client): begin e2e test that creates a room

--- packages/client/e2e/home.spec.ts
+++ packages/client/e2e/home.spec.ts
@@ -1,7 +1,66 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    const title = page.locator('.navbar__inner .navbar__title');
-    await expect(title).toHaveText('Playwright');
+test('Room creation', async ({ page }) => {
+    await page.goto('/');
+
+    const focusTrap = page.locator('text="Click"').first();
+    await focusTrap.click();
+
+    await expect(page.locator('text="Home"').first()).toBeVisible();
+
+    const goToTracksSearch = page.locator('text="Search"');
+    await goToTracksSearch.click();
+
+    const trackQuery = 'BB Brunes';
+    await page.fill('css=[placeholder*="Search a track"]', trackQuery);
+    await page.keyboard.press('Enter');
+
+    await expect(page.locator('text="Results"')).toBeVisible();
+
+    const firstMatchingSong = page.locator('text=BB Brunes').first();
+    await expect(firstMatchingSong).toBeVisible();
+
+    await firstMatchingSong.click();
+
+    await expect(
+        page.locator('text="What is the name of the room?"'),
+    ).toBeVisible();
+
+    const roomName = 'MusicRoom is the best';
+    await page.fill('css=[placeholder="Francis Cabrel OnlyFans"]', roomName);
+    await page.click('text="Next" >> visible=true');
+
+    await expect(
+        page.locator('text="What is the opening status of the room?"'),
+    ).toBeVisible();
+
+    const publicMode = page.locator(
+        'css=[aria-selected="true"] >> text="Public"',
+    );
+    await expect(publicMode).toBeVisible();
+    await page.click('text="Next" >> visible=true');
+
+    const noVotingRestriction = page.locator(
+        'css=[aria-selected="true"] >> text="No restriction"',
+    );
+    await expect(noVotingRestriction).toBeVisible();
+    await page.click('text="Next" >> visible=true');
+
+    const broadcastMode = page.locator(
+        'css=[aria-selected="true"] >> text="Broadcast"',
+    );
+    await expect(broadcastMode).toBeVisible();
+    await page.click('text="Next" >> visible=true');
+
+    const smallestVotesConstraint = page.locator(
+        `css=[aria-selected="true"] >> text="Party at Kitty and Stud's"`,
+    );
+    await expect(smallestVotesConstraint).toBeVisible();
+    await page.click('text="Next" >> visible=true');
+
+    await expect(page.locator('text="Confirm room creation"')).toBeVisible();
+    const selectedSongTitle = await firstMatchingSong.textContent();
+    await expect(page.locator(`text=${selectedSongTitle}`)).toBeVisible();
+
+    await page.waitForTimeout(100_000);
 });

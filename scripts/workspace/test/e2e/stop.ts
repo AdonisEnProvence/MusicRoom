cb046863afa55e9421957ea928e3ae0df3406e46
Devessier <baptiste@devessier.fr>
2021-11-01 09:53:15 +0100

    build: use docker-compose command instead of Docker Desktop

--- scripts/workspace/test/e2e/stop.ts
+++ scripts/workspace/test/e2e/stop.ts
@@ -3,12 +3,12 @@ import { $, cd } from 'zx';
 
 async function stopTemporalDockerCompose() {
     cd('packages/temporal/docker-compose');
-    await $`docker compose down`;
+    await $`docker-compose down`;
 }
 
 async function stopServerDockerCompose() {
     cd('packages/server');
-    await $`docker compose down`;
+    await $`docker-compose down`;
 }
 
 async function stopServices() {

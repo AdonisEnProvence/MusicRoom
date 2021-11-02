c7988089d7e3e8881f2566b5cfe8d2028cbd05f4
prastoin <tkesray@gmail.com>
2021-11-02 13:06:14 +0100

    feat(client): test B implementation wip

---
+++ packages/client/e2e/testB.spec.ts
@@ -0,0 +1,332 @@
+import { test, expect, Browser, Page, Locator } from '@playwright/test';
+
+function assertIsNotUndefined<ValueType>(
+    value: ValueType | undefined,
+): asserts value is ValueType {
+    if (value === undefined) {
+        throw new Error('value must not be undefined');
+    }
+}
+
+const AVAILABLE_USERS_LIST = [
+    '8d71dcb3-9638-4b7a-89ad-838e2310686c',
+    '71bc3025-b765-4f84-928d-b4dca8871370',
+    'd125ecde-b0ee-4ab8-a488-c0e7a8dac7c5',
+    '7f4bc598-c5be-4412-acc4-515a87b797e7',
+];
+
+type SetupAndGetUserContextArgs = {
+    browser: Browser;
+    userIndex: number;
+};
+async function setupAndGetUserContext({
+    browser,
+    userIndex,
+}: SetupAndGetUserContextArgs): Promise<{ page: Page }> {
+    const joinerContext = await browser.newContext({
+        storageState: {
+            cookies: [],
+            origins: [
+                {
+                    origin: 'http://localhost:4000',
+                    localStorage: [
+                        {
+                            name: 'USER_ID',
+                            value: AVAILABLE_USERS_LIST[userIndex],
+                        },
+                    ],
+                },
+            ],
+        },
+    });
+    const page = await joinerContext.newPage();
+
+    await page.goto('/');
+
+    return { page };
+}
+
+type FindMiniPlayerWithRoomNameAndGoFullscreenArgs = {
+    roomName: string;
+    page: Page;
+};
+async function findMiniPlayerWithRoomNameAndGoFullscreen({
+    page,
+    roomName,
+}: FindMiniPlayerWithRoomNameAndGoFullscreenArgs) {
+    const miniPlayerWithRoomName = page.locator(`text="${roomName}"`).first();
+    await expect(miniPlayerWithRoomName).toBeVisible();
+    await miniPlayerWithRoomName.click();
+}
+
+type CreateRoomArgs = { creatorPage: Page };
+async function createDirectRoomAndGoFullscreen({
+    creatorPage,
+}: CreateRoomArgs) {
+    const focusTrap = creatorPage.locator('text="Click"').first();
+    await focusTrap.click();
+
+    await expect(creatorPage.locator('text="Home"').first()).toBeVisible();
+
+    //Searching for a track
+    const goToTracksSearch = creatorPage.locator('text="Search"');
+    await goToTracksSearch.click();
+
+    const trackQuery = 'BB Brunes';
+    await creatorPage.fill(
+        'css=[placeholder*="Search a track"] >> visible=true',
+        trackQuery,
+    );
+    await creatorPage.keyboard.press('Enter');
+
+    await expect(creatorPage.locator('text="Results"')).toBeVisible();
+
+    const firstMatchingSong = creatorPage.locator('text=BB Brunes').first();
+    const selectedSongTitle = await firstMatchingSong.textContent();
+    if (selectedSongTitle === null) {
+        throw new Error('SelectedSongTitle is null');
+    }
+    await expect(firstMatchingSong).toBeVisible();
+
+    await firstMatchingSong.click();
+
+    /** Entering the mtv room creation form **/
+    //RoomName
+    await expect(
+        creatorPage.locator('text="What is the name of the room?"'),
+    ).toBeVisible();
+
+    const roomName = 'MusicRoom is the best';
+    await creatorPage.fill(
+        'css=[placeholder="Francis Cabrel OnlyFans"]',
+        roomName,
+    );
+    await creatorPage.click('text="Next" >> visible=true');
+
+    //Room isOpen
+    await expect(
+        creatorPage.locator('text="What is the opening status of the room?"'),
+    ).toBeVisible();
+
+    const publicMode = creatorPage.locator(
+        'css=[aria-selected="true"] >> text="Public"',
+    );
+    await expect(publicMode).toBeVisible();
+    await creatorPage.click('text="Next" >> visible=true');
+
+    //Voting restrictions
+    const noVotingRestriction = creatorPage.locator(
+        'css=[aria-selected="true"] >> text="No restriction"',
+    );
+    await expect(noVotingRestriction).toBeVisible();
+    await creatorPage.click('text="Next" >> visible=true');
+
+    //Room mode
+    const directMode = creatorPage.locator(
+        'css=[aria-selected="false"] >> text="Direct"',
+    );
+    await expect(directMode).toBeVisible();
+    await directMode.click();
+    await creatorPage.click('text="Next" >> visible=true');
+
+    //Minimum count to be played
+    const smallestVotesConstraint = creatorPage.locator(
+        `css=[aria-selected="false"] >> text="Friendly online event"`,
+    );
+    await expect(smallestVotesConstraint).toBeVisible();
+    await smallestVotesConstraint.click();
+    await creatorPage.click('text="Next" >> visible=true');
+
+    //Confirmation
+    await expect(
+        creatorPage.locator('text="Confirm room creation"'),
+    ).toBeVisible();
+    const elementWithSelectedSongTitle = creatorPage.locator(
+        `text=${selectedSongTitle}`,
+    );
+    await expect(elementWithSelectedSongTitle).toBeVisible();
+
+    await creatorPage.click('text="Next" >> visible=true');
+    ///
+
+    // We expect creation form to have disappeared
+    // and user to have not been redirected to another screen than
+    // the one in which she opened the form.
+    await expect(creatorPage.locator('text="Results"').first()).toBeVisible();
+
+    await findMiniPlayerWithRoomNameAndGoFullscreen({
+        page: creatorPage,
+        roomName,
+    });
+
+    return {
+        roomName,
+        selectedSongTitle,
+    };
+}
+
+type JoinGivenRoomAndGoFullscreenArgs = {
+    joiningUserPage: Page;
+    roomName: string;
+    expectedListenersCounterValue: number;
+};
+async function userJoinsGivenRoomAndGoFullscreen({
+    joiningUserPage,
+    roomName,
+    expectedListenersCounterValue,
+}: JoinGivenRoomAndGoFullscreenArgs) {
+    const focusTrap = joiningUserPage.locator('text="Click"').first();
+    await focusTrap.click();
+
+    await joiningUserPage.click('text="Go to Music Track Vote"');
+
+    // Code to use infinite scroll
+    let matchingRoom: Locator | undefined;
+    let hasFoundRoom = false;
+
+    await joiningUserPage.mouse.move(
+        (joiningUserPage.viewportSize()?.width ?? 0) / 2,
+        150,
+    );
+    while (hasFoundRoom === false) {
+        await joiningUserPage.mouse.wheel(0, 999999);
+
+        matchingRoom = joiningUserPage.locator(`text="${roomName}"`).first();
+        const isMatchingRoomVisible = await matchingRoom.isVisible();
+        if (isMatchingRoomVisible === false) {
+            hasFoundRoom = false;
+
+            continue;
+        }
+
+        hasFoundRoom = true;
+    }
+    assertIsNotUndefined(matchingRoom);
+
+    await expect(matchingRoom).toBeVisible();
+
+    await matchingRoom.click();
+
+    await joiningUserPage.click('css=[aria-label="Go back"] >> visible=true');
+
+    await findMiniPlayerWithRoomNameAndGoFullscreen({
+        page: joiningUserPage,
+        roomName,
+    });
+
+    const expectedListenersCounterAriaLabel = `${expectedListenersCounterValue} Listeners`;
+    await expect(
+        joiningUserPage.locator(
+            `text="${expectedListenersCounterAriaLabel}" >> visible=true`,
+        ),
+    ).toBeVisible();
+}
+
+type UserVoteForGivenTrackArgs = {
+    page: Page;
+    trackName: string;
+};
+/**
+ * /!\ The page has to be in music player fullscreen before calling this function /!\
+ */
+async function userVoteForGivenTrackFromFullscreen({
+    page,
+    trackName,
+}: UserVoteForGivenTrackArgs) {
+    await page.waitForTimeout(10000);
+
+    const trackItem = page.locator(`text="${trackName}" >> visible=true`);
+    await expect(trackItem).toBeVisible();
+    await expect(trackItem).toBeEnabled();
+    await trackItem.click();
+    await expect(trackItem).toBeDisabled();
+}
+
+/**
+ * /!\ The page has to be in music player fullscreen before calling this function /!\
+ */
+async function userSuggestATrackFromFullscreen({
+    page,
+    trackName,
+}: UserVoteForGivenTrackArgs) {
+    const suggestTrackButton = page.locator(
+        'css=[aria-label="Suggest a track"] >> visible=true',
+    );
+    await expect(suggestTrackButton).toBeVisible();
+    await expect(suggestTrackButton).toBeEnabled();
+    await suggestTrackButton.click();
+
+    await page.fill(
+        'css=[placeholder*="Search a track"] >> visible=true',
+        trackName,
+    );
+    await page.keyboard.press('Enter');
+
+    await expect(page.locator('text="Results"')).toBeVisible();
+
+    const firstMatchingSong = page.locator(`text=${trackName}`).first();
+    await expect(firstMatchingSong).toBeVisible();
+    const selectedSongTitle = await firstMatchingSong.textContent();
+    if (selectedSongTitle === null) {
+        throw new Error('SelectedSongTitle is null');
+    }
+    await expect(firstMatchingSong).toBeVisible();
+
+    await firstMatchingSong.click();
+    return { selectedSongTitle };
+}
+
+test('Test B see following link for more information: https://3.basecamp.com/4704981/buckets/22220886/messages/4292491228#:~:text=Test%20end-,Test%20B/,-UserA%20Section%20full', async ({
+    browser,
+}) => {
+    let userIndex = 0;
+    const [
+        { page: creatorUserA },
+        { page: joiningUserB },
+        { page: joiningUserC },
+    ] = await Promise.all([
+        setupAndGetUserContext({
+            browser,
+            userIndex: userIndex++,
+        }),
+        setupAndGetUserContext({ browser, userIndex: userIndex++ }),
+        setupAndGetUserContext({ browser, userIndex: userIndex++ }),
+    ]);
+
+    const { roomName, selectedSongTitle } =
+        await createDirectRoomAndGoFullscreen({
+            creatorPage: creatorUserA,
+        });
+
+    await userJoinsGivenRoomAndGoFullscreen({
+        joiningUserPage: joiningUserB,
+        roomName,
+        expectedListenersCounterValue: 2,
+    });
+    await userJoinsGivenRoomAndGoFullscreen({
+        joiningUserPage: joiningUserC,
+        roomName,
+        expectedListenersCounterValue: 3,
+    });
+
+    const suggestedTrackName = 'poussin piou';
+
+    const { selectedSongTitle: suggestedSongTitle } =
+        await userSuggestATrackFromFullscreen({
+            page: creatorUserA,
+            trackName: suggestedTrackName,
+        });
+    await expect(
+        joiningUserB.locator(`text="${suggestedSongTitle}" >> visible=true`),
+    ).toBeVisible();
+    await expect(
+        joiningUserC.locator(`text="${suggestedSongTitle}" >> visible=true`),
+    ).toBeVisible();
+
+    await userVoteForGivenTrackFromFullscreen({
+        page: joiningUserB,
+        trackName: selectedSongTitle,
+    });
+
+    await creatorUserA.waitForTimeout(100_000);
+});

c8ba66f359ea812bfcf49eb110723889845fb629
prastoin <tkesray@gmail.com>
2021-12-02 16:31:30 +0100

    refactor(temporal): update every activity endpoint to contain mtv keyword w ADONIS_MTV_ENDPOINT

--- packages/temporal/activities/fetch.go
+++ packages/temporal/activities/fetch.go
@@ -3,5 +3,6 @@ package activities
 import "os"
 
 var (
-	ADONIS_ENDPOINT = os.Getenv("ADONIS_ENDPOINT")
+	ADONIS_ENDPOINT     = os.Getenv("ADONIS_ENDPOINT")
+	ADONIS_MTV_ENDPOINT = ADONIS_ENDPOINT + "/temporal/mtv"
 )

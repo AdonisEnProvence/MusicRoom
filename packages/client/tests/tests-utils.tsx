736d3d2aab5a92e787c7393c9148185bf30eadb6
Devessier <baptiste@devessier.fr>
2021-12-03 12:36:10 +0100

    test(client): create a function to ease rendering the Application in tests

--- packages/client/tests/tests-utils.tsx
+++ packages/client/tests/tests-utils.tsx
@@ -4,6 +4,7 @@ import {
     render as rtlRender,
     RenderAPI,
     RenderOptions,
+    waitFor,
 } from '@testing-library/react-native';
 import { DripsyProvider } from 'dripsy';
 import { datatype } from 'faker';
@@ -12,6 +13,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppContextProvider } from '../contexts/AppContext';
 import { SocketContextProvider } from '../contexts/SocketContext';
 import { useTheme } from '../hooks/useTheme';
+import Navigation from '../navigation';
 import { ServerSocket, serverSocket } from '../services/websockets';
 
 export type SizeTerms = 'xs' | 's' | 'm' | 'l' | 'xl';
@@ -55,6 +57,26 @@ export function render(
         ...utils,
         serverSocket,
     };
+}
+
+/**
+ * Renders the application <Navigation /> component.
+ * It renders asynchronously the first screen.
+ * This function waits for the home screen to be rendered before returning.
+ */
+export async function renderApp(
+    options?: RenderOptions,
+): Promise<RenderAPI & { serverSocket: ServerSocket }> {
+    const screen = render(
+        <Navigation colorScheme="dark" toggleColorScheme={noop} />,
+        options,
+    );
+
+    await waitFor(() => {
+        expect(screen.getAllByText(/home/i).length).toBeGreaterThanOrEqual(1);
+    });
+
+    return screen;
 }
 
 export function waitForTimeout(ms: number): Promise<void> {

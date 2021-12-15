9323cb58885a9e850ad9d1976f954124eef5a952
Devessier <baptiste@devessier.fr>
2021-12-15 09:50:16 +0100

    feat(temporal): create test to ensure creator can delete tracks in rooms with invitation

--- packages/temporal/mpe/workflows/mpe_delete_tracks_test.go
+++ packages/temporal/mpe/workflows/mpe_delete_tracks_test.go
@@ -257,6 +257,89 @@ func (s *DeleteTracksTestSuite) Test_AcknowledgeDeletionEvenWhenNoTracksWereDele
 	s.ErrorIs(err, workflow.ErrDeadlineExceeded, "The workflow ran on an infinite loop")
 }
 
+func (s *DeleteTracksTestSuite) Test_InRoomWithConstraintsCreatorCanDeleteTracks() {
+	initialTracksIDs := []string{
+		faker.UUIDHyphenated(),
+		faker.UUIDHyphenated(),
+	}
+	params, roomCreatorDeviceID := s.getWorkflowInitParams(initialTracksIDs)
+	params.IsOpenOnlyInvitedUsersCanEdit = true
+	var a *activities_mpe.Activities
+
+	initialTracksMetadata := []shared.TrackMetadata{
+		{
+			ID:         initialTracksIDs[0],
+			Title:      faker.Word(),
+			ArtistName: faker.Name(),
+			Duration:   42000,
+		},
+		{
+			ID:         initialTracksIDs[1],
+			Title:      faker.Word(),
+			ArtistName: faker.Name(),
+			Duration:   42000,
+		},
+	}
+
+	tick := 1 * time.Millisecond
+	resetMock, registerDelayedCallbackWrapper := s.initTestEnv()
+
+	defer resetMock()
+
+	// Common activities calls
+	s.env.OnActivity(
+		a.MpeCreationAcknowledgementActivity,
+		mock.Anything,
+		mock.Anything,
+	).Return(nil).Once()
+	s.env.OnActivity(
+		activities.FetchTracksInformationActivity,
+		mock.Anything,
+		initialTracksIDs,
+	).Return(initialTracksMetadata, nil).Once()
+
+	// Specific activities calls
+	s.env.OnActivity(
+		a.AcknowledgeDeletingTracksActivity,
+		mock.Anything,
+		mock.Anything,
+	).Return(nil).Once()
+
+	initialTracksFetched := tick * 200
+	registerDelayedCallbackWrapper(func() {
+		mpeState := s.getMpeState(shared_mpe.NoRelatedUserID)
+
+		s.Equal(initialTracksMetadata, mpeState.Tracks)
+	}, initialTracksFetched)
+
+	creatorDeletesTracks := tick * 200
+	registerDelayedCallbackWrapper(func() {
+		s.emitDeleteTracksSignal(shared_mpe.NewDeleteTracksSignalArgs{
+			TracksIDs: initialTracksIDs,
+			UserID:    params.RoomCreatorUserID,
+			DeviceID:  roomCreatorDeviceID,
+		})
+	}, creatorDeletesTracks)
+
+	checkDeletedTracks := tick * 200
+	registerDelayedCallbackWrapper(func() {
+		mpeState := s.getMpeState(shared_mpe.NoRelatedUserID)
+
+		s.Empty(mpeState.Tracks)
+	}, checkDeletedTracks)
+
+	s.env.ExecuteWorkflow(MpeRoomWorkflow, params)
+
+	s.True(s.env.IsWorkflowCompleted())
+	err := s.env.GetWorkflowError()
+	s.ErrorIs(err, workflow.ErrDeadlineExceeded, "The workflow ran on an infinite loop")
+}
+
+// TODO: to implement when joining room is implemented
+func (s *DeleteTracksTestSuite) Test_InRoomWithConstraintsOnlyInvitedUsersCanDeleteTracks() {
+
+}
+
 func TestDeleteTracksTestSuite(t *testing.T) {
 	suite.Run(t, new(DeleteTracksTestSuite))
 }

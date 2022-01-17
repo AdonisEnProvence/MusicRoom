2dff92d1fa6ab73e3882b76cacef8e2b13875851
prastoin <tkesray@gmail.com>
2022-01-17 14:23:07 +0100

    refactor(client): update e2e tests to new on join room screens successions

--- packages/client/e2e/mtv/test-d.spec.ts
+++ packages/client/e2e/mtv/test-d.spec.ts
@@ -159,14 +159,12 @@ async function joinRoom({ page, roomName }: { page: Page; roomName: string }) {
 
     await matchingRoom.click();
 
-    // Close MTV Search
-    await page.click('css=[aria-label="Go back"] >> visible=true');
-
-    // Open player full screen
-    const miniPlayerWithRoomName = page.locator(`text="${roomName}"`).first();
-    await expect(miniPlayerWithRoomName).toBeVisible();
-
-    await miniPlayerWithRoomName.click();
+    const expectedListenersCounterAriaLabel = `2 Listeners`;
+    await expect(
+        page.locator(
+            `text="${expectedListenersCounterAriaLabel}" >> visible=true`,
+        ),
+    ).toBeVisible();
 }
 
 async function playTrack(page: Page) {

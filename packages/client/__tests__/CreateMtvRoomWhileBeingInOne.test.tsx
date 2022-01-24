b2a3a8df61b80871a1da25070f89e2c0c87fe0d3
Devessier <baptiste@devessier.fr>
2022-01-24 13:29:37 +0100

    test(client): test that music player context is not refreshed when exiting mtv room creation

--- packages/client/__tests__/CreateMtvRoomWhileBeingInOne.test.tsx
+++ packages/client/__tests__/CreateMtvRoomWhileBeingInOne.test.tsx
@@ -248,3 +248,135 @@ test('Device should stop playing when exiting current room while being in MTV Cr
         expect(musicPlayer).toBeNull();
     });
 });
+
+test('Music Player context is not refreshed when exiting MTV Room Creation Form', async () => {
+    const fakeTrack = db.searchableTracks.create();
+    const tracksList = [generateTrackMetadata(), generateTrackMetadata()];
+    const thisDevice = {
+        deviceID: datatype.uuid(),
+        name: random.word(),
+    };
+    const userID = datatype.uuid();
+    const state: MtvWorkflowState | undefined = {
+        roomID: datatype.uuid(),
+        name: random.word(),
+        playing: true,
+        usersLength: 1,
+        playingMode: 'BROADCAST',
+        isOpen: true,
+        isOpenOnlyInvitedUsersCanVote: false,
+        hasTimeAndPositionConstraints: false,
+        timeConstraintIsValid: null,
+        delegationOwnerUserID: null,
+        userRelatedInformation: {
+            hasControlAndDelegationPermission: true,
+            userFitsPositionConstraint: null,
+            userHasBeenInvited: false,
+            emittingDeviceID: thisDevice.deviceID,
+            userID,
+            tracksVotedFor: [],
+        },
+        currentTrack: {
+            ...tracksList[0],
+            elapsed: 0,
+        },
+        tracks: tracksList.slice(1),
+        roomCreatorUserID: userID,
+        minimumScoreToBePlayed: 1,
+    };
+
+    serverSocket.on('GET_CONNECTED_DEVICES_AND_DEVICE_ID', (cb) => {
+        cb({
+            currDeviceID: thisDevice.deviceID,
+            devices: [
+                {
+                    deviceID: thisDevice.deviceID,
+                    name: 'thisDevice',
+                },
+            ],
+        });
+    });
+
+    const mtvGetContextSpy = jest.fn(() => {
+        if (state !== undefined) {
+            serverSocket.emit('MTV_RETRIEVE_CONTEXT', state);
+        }
+    });
+    serverSocket.on('MTV_GET_CONTEXT', mtvGetContextSpy);
+
+    const screen = await renderApp();
+
+    expect(screen.getAllByText(/home/i).length).toBeGreaterThanOrEqual(1);
+
+    const musicPlayerMini = screen.getByTestId('music-player-mini');
+    expect(musicPlayerMini).toBeTruthy();
+
+    const miniPlayerTrackTitle = await within(musicPlayerMini).findByText(
+        new RegExp(`${tracksList[0].title}.*${tracksList[0].artistName}`),
+    );
+    expect(miniPlayerTrackTitle).toBeTruthy();
+
+    expect(screen.getAllByText(/home/i).length).toBeGreaterThanOrEqual(1);
+
+    expect(
+        screen.getByTestId('music-player-playing-device-emitting'),
+    ).toBeTruthy();
+
+    //Search steps
+    const searchScreenLink = screen.getByText(/^search$/i);
+    expect(searchScreenLink).toBeTruthy();
+
+    fireEvent.press(searchScreenLink);
+
+    await waitFor(() =>
+        expect(screen.getByText(/search.*track/i)).toBeTruthy(),
+    );
+
+    const searchInput = await screen.findByPlaceholderText(/search.*track/i);
+    expect(searchInput).toBeTruthy();
+
+    const SEARCH_QUERY = fakeTrack.title.slice(0, 3);
+
+    /**
+     * To simulate a real interaction with a text input, we need to:
+     * 1. Focus it
+     * 2. Change its text
+     * 3. Submit the changes
+     */
+    fireEvent(searchInput, 'focus');
+    fireEvent.changeText(searchInput, SEARCH_QUERY);
+    fireEvent(searchInput, 'submitEditing');
+
+    const trackResultListItem = await screen.findByText(fakeTrack.title);
+    expect(trackResultListItem).toBeTruthy();
+
+    fireEvent.press(trackResultListItem);
+
+    const creationModal = await screen.findByText(/what.*to.*do.*track/i);
+    expect(creationModal).toBeTruthy();
+
+    const createMtvRoomButton = screen.getByText(/create.*mtv/i);
+    expect(createMtvRoomButton).toBeTruthy();
+
+    fireEvent.press(createMtvRoomButton);
+
+    await waitFor(() => {
+        const roomCreationFormFirstStepTitle =
+            screen.getByText(/what.*is.*name.*room/i);
+        expect(roomCreationFormFirstStepTitle).toBeTruthy();
+    });
+
+    // Go back
+    const backButton = screen.getByText(/back/i);
+    expect(backButton).toBeTruthy();
+
+    fireEvent.press(backButton);
+
+    await waitFor(() => {
+        const roomCreationFormFirstStepTitle =
+            screen.queryByText(/what.*is.*name.*room/i);
+        expect(roomCreationFormFirstStepTitle).toBeNull();
+    });
+
+    expect(mtvGetContextSpy).toHaveBeenCalledTimes(1);
+});

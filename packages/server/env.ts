f88b9a1233483d62c64772624d3654fbb76e4812
prastoin <tkesray@gmail.com>
2022-03-02 16:31:52 +0100

    feat(server): install & set up adonis/session

--- packages/server/env.ts
+++ packages/server/env.ts
@@ -40,4 +40,5 @@ export default Env.rules({
     REDIS_HOST: Env.schema.string({ format: 'host' }),
     REDIS_PORT: Env.schema.number(),
     REDIS_PASSWORD: Env.schema.string.optional(),
+    SESSION_DRIVER: Env.schema.string(),
 });

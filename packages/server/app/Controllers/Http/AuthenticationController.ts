ec4f793df7906ab3e32750a29517404fea2eabe9
prastoin <tkesray@gmail.com>
2022-03-03 16:46:38 +0100

    feat(server): handle sign up web auth and api auth & tests

--- packages/server/app/Controllers/Http/AuthenticationController.ts
+++ packages/server/app/Controllers/Http/AuthenticationController.ts
@@ -1,9 +1,9 @@
 import { SignUpResponseBody } from '@musicroom/types';
 import User from 'App/Models/User';
-import { datatype, internet } from 'faker';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import * as z from 'zod';
 import { OpaqueTokenContract } from '@ioc:Adonis/Addons/Auth';
+import { SignUpRequestBody } from '@musicroom/types/src/authentication';
 
 export const SignInRequestBody = z.object({
     email: z.string().email(),
@@ -13,21 +13,83 @@ export const SignInRequestBody = z.object({
 export type SignInRequestBody = z.infer<typeof SignInRequestBody>;
 
 export default class AuthenticationController {
-    public async signUp(): Promise<SignUpResponseBody> {
-        const userID = datatype.uuid();
-        const userNickname = internet.userName();
+    public async signUp({
+        request,
+        response,
+        auth,
+    }: HttpContextContract): Promise<SignUpResponseBody> {
+        const { authenticationMode, email, password, userNickname } =
+            SignUpRequestBody.parse(request.body());
+
+        const userWithGivenNickname = await User.findBy(
+            'nickname',
+            userNickname,
+        );
+        console.log({ userWithGivenNickname });
+        if (userWithGivenNickname) {
+            response.status(400);
+            return {
+                status: 'SAME_NICKNAME',
+            };
+        }
+
+        const emailIsValid = true;
+        const emailIsInvalid = !emailIsValid;
+        if (emailIsInvalid) {
+            return {
+                status: 'BAD_EMAIL',
+            };
+        }
+
+        const userWithGivenEmail = await User.findBy('email', email);
+        console.log({ userWithGivenEmail });
+        if (userWithGivenEmail) {
+            response.status(400);
+            return {
+                status: 'SAME_EMAIL',
+            };
+        }
+
+        const passwordIsStrong = true;
+        const passwordIsNotString = !passwordIsStrong;
+        console.log({ passwordIsNotString });
+        if (passwordIsNotString) {
+            response.status(400);
+            return {
+                status: 'WEAK_PASSWORD',
+            };
+        }
 
         await User.create({
-            uuid: userID,
             nickname: userNickname,
-            email: internet.email(),
-            password: internet.password(),
+            email: email,
+            password,
         });
 
-        return {
-            userID,
-            userNickname,
-        };
+        switch (authenticationMode) {
+            case 'api': {
+                const { token } = await auth
+                    .use('api')
+                    .attempt(email, password);
+
+                return {
+                    token,
+                    status: 'SUCCESS',
+                };
+            }
+            case 'web': {
+                await auth.use('web').attempt(email, password);
+
+                return {
+                    status: 'SUCCESS',
+                };
+            }
+            default: {
+                throw new Error(
+                    `unknown authentication mode encountered ${authenticationMode}`,
+                );
+            }
+        }
     }
 
     public async signIn({
--- packages/types/src/authentication.ts
+++ packages/types/src/authentication.ts
@@ -1,7 +1,25 @@
 import * as z from 'zod';
 
+export const AuthenticationModeValues = z.enum(['web', 'api']);
+export type AuthenticationModeValues = z.infer<typeof AuthenticationModeValues>;
+
 export const SignUpResponseBody = z.object({
-    userID: z.string().uuid(),
-    userNickname: z.string(),
+    token: z.string().optional(),
+    status: z.enum([
+        'SAME_NICKNAME',
+        'SUCCESS',
+        'SAME_EMAIL',
+        'BAD_EMAIL',
+        'WEAK_PASSWORD',
+    ]),
 });
 export type SignUpResponseBody = z.infer<typeof SignUpResponseBody>;
+
+export const SignUpRequestBody = z.object({
+    userNickname: z.string(),
+    password: z.string(),
+    email: z.string().email(),
+    authenticationMode: AuthenticationModeValues,
+});
+
+export type SignUpRequestBody = z.infer<typeof SignUpRequestBody>;

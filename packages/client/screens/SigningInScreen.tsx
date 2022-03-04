c2878107dd27bd07ac6943284740bd78f9c5c5ee
Devessier <baptiste@devessier.fr>
2022-03-04 13:04:28 +0100

    feat(client): improve signining form design

--- packages/client/screens/SigningInScreen.tsx
+++ packages/client/screens/SigningInScreen.tsx
@@ -1,17 +1,12 @@
-import { Button } from 'dripsy';
+import { SafeAreaView, Text, useSx, View } from 'dripsy';
 import React, { useState } from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import {
-    AppScreen,
-    AppScreenContainer,
-    AppScreenHeader,
-    TextField,
-} from '../components/kit';
+import { TouchableOpacity } from 'react-native';
+import { AppScreen, TextField } from '../components/kit';
 import { useAppContext } from '../contexts/AppContext';
 import { SigningInScreenProps } from '../types';
 
 const SigningInScreen: React.FC<SigningInScreenProps> = () => {
-    const insets = useSafeAreaInsets();
+    const sx = useSx();
     const { appService } = useAppContext();
     const [email, setEmail] = useState('devessier@devessier.fr');
     const [password, setPassword] = useState('devessierBgDu13');
@@ -26,25 +21,120 @@ const SigningInScreen: React.FC<SigningInScreenProps> = () => {
 
     return (
         <AppScreen>
-            <AppScreenHeader title="Signing in" insetTop={insets.top} />
+            <SafeAreaView sx={{ flex: 1 }}>
+                <Text
+                    sx={{
+                        color: 'white',
+                        fontSize: 'l',
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                        marginX: 'auto',
+                        marginTop: 'xl',
+                    }}
+                >
+                    MusicRoom
+                </Text>
 
-            <AppScreenContainer>
-                <TextField
-                    value={email}
-                    onChangeText={setEmail}
-                    placeholder="Email"
-                    placeholderTextColor="#fff"
-                />
+                <View
+                    sx={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <View
+                        sx={{
+                            width: 520,
+                            flexShrink: 1,
+                        }}
+                    >
+                        <View sx={{ paddingX: 'l' }}>
+                            <Text
+                                sx={{
+                                    color: 'white',
+                                    fontSize: 'm',
+                                    fontWeight: 'bold',
+                                    textAlign: 'center',
+                                    marginBottom: 'xl',
+                                }}
+                            >
+                                Welcome back Popol!
+                            </Text>
 
-                <TextField
-                    value={password}
-                    onChangeText={setPassword}
-                    placeholder="Password"
-                    placeholderTextColor="#fff"
-                />
+                            <View sx={{ marginBottom: 'xl' }}>
+                                <Text
+                                    sx={{
+                                        color: 'greyLighter',
+                                        fontSize: 'xs',
+                                        fontWeight: 'bold',
+                                        textAlign: 'left',
+                                        marginBottom: 'm',
+                                    }}
+                                >
+                                    Nickname
+                                </Text>
 
-                <Button title="Submit" onPress={handleSigningInSubmit} />
-            </AppScreenContainer>
+                                <TextField
+                                    value={email}
+                                    onChangeText={setEmail}
+                                    placeholder="Email"
+                                    placeholderTextColor="#fff"
+                                    sx={{
+                                        borderWidth: 1,
+                                        borderColor: 'greyLighter',
+                                    }}
+                                />
+                            </View>
+
+                            <View sx={{ marginBottom: 'xl' }}>
+                                <Text
+                                    sx={{
+                                        color: 'greyLighter',
+                                        fontSize: 'xs',
+                                        fontWeight: 'bold',
+                                        textAlign: 'left',
+                                        marginBottom: 'm',
+                                    }}
+                                >
+                                    Nickname
+                                </Text>
+
+                                <TextField
+                                    value={password}
+                                    onChangeText={setPassword}
+                                    placeholder="Password"
+                                    placeholderTextColor="#fff"
+                                    sx={{
+                                        borderWidth: 1,
+                                        borderColor: 'greyLighter',
+                                    }}
+                                />
+                            </View>
+
+                            <TouchableOpacity
+                                onPress={handleSigningInSubmit}
+                                style={sx({
+                                    paddingX: 's',
+                                    paddingY: 'm',
+                                    backgroundColor: 'greyLighter',
+                                    borderRadius: 's',
+                                })}
+                            >
+                                <Text
+                                    sx={{
+                                        color: 'greyLight',
+                                        textAlign: 'center',
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    Log in
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </SafeAreaView>
         </AppScreen>
     );
 };

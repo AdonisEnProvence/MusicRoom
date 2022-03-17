078c2a38f5cdd81a322524f7097ec1ea62b2f97d
prastoin <tkesray@gmail.com>
2022-03-17 14:58:58 +0100

    test(client): e2e test sign up and sign up on browser with multiples tabs

--- packages/client/e2e/sign-in.spec.ts
+++ packages/client/e2e/sign-in.spec.ts
@@ -57,3 +57,68 @@ test('Signs up a user, expects to be redirected to home and to be still loggged 
 
     await pageIsOnHomeScreen({ page: newTab });
 });
+
+test('It should renders home on every browser tab after a signIn', async ({
+    browser,
+}) => {
+    const { email, password } = await setupPageAndSignUpUser({
+        browser,
+
+        knownSearches,
+    });
+
+    const context = await browser.newContext({
+        permissions: ['geolocation'],
+        geolocation: GEOLOCATION_POSITIONS['Manosque, France'],
+    });
+    const secondContext = await browser.newContext({
+        permissions: ['geolocation'],
+        geolocation: GEOLOCATION_POSITIONS['Manosque, France'],
+    });
+
+    await mockSearchTracks({
+        context: context,
+        knownSearches,
+    });
+
+    const page = await context.newPage();
+    await page.goto('/');
+
+    const newTab = await context.newPage();
+    await newTab.goto('/');
+
+    const newTab1 = await context.newPage();
+    await newTab1.goto('/');
+
+    const newTab2 = await context.newPage();
+    await newTab2.goto('/');
+
+    const secondContextPage = await secondContext.newPage();
+    await secondContextPage.goto('/');
+
+    const emailInput = page.locator(
+        withinSignInFormScreenContainer(`css=[placeholder="Email"]`),
+    );
+    await expect(emailInput).toBeVisible();
+    await emailInput.fill(email);
+
+    const passwordInput = page.locator(
+        withinSignInFormScreenContainer(`css=[placeholder="Password"]`),
+    );
+    await expect(passwordInput).toBeVisible();
+    await passwordInput.fill(password);
+
+    const submitSignInFormButton = page.locator(
+        withinSignInFormScreenContainer(`text="Log in"`),
+    );
+    await expect(submitSignInFormButton).toBeVisible();
+    await submitSignInFormButton.click();
+
+    await pageIsOnHomeScreen({ page });
+    await pageIsOnHomeScreen({ page: newTab });
+    await pageIsOnHomeScreen({ page: newTab1 });
+    await pageIsOnHomeScreen({ page: newTab2 });
+    await expect(
+        secondContextPage.locator('text="Or sign up ?"'),
+    ).toBeVisible();
+});
--- packages/client/e2e/sign-up.spec.ts
+++ packages/client/e2e/sign-up.spec.ts
@@ -42,3 +42,51 @@ test('Signs up a user, expects to be redirected to home and to be still loggged 
 
     await pageIsOnHomeScreen({ page: newTab });
 });
+
+test('It should renders home on every browser tab after a signUp', async ({
+    browser,
+}) => {
+    const context = await browser.newContext({
+        permissions: ['geolocation'],
+        geolocation: GEOLOCATION_POSITIONS['Manosque, France'],
+    });
+    const secondContext = await browser.newContext({
+        permissions: ['geolocation'],
+        geolocation: GEOLOCATION_POSITIONS['Manosque, France'],
+    });
+    await mockSearchTracks({
+        context: context,
+        knownSearches,
+    });
+
+    const page = await context.newPage();
+    await page.goto('/');
+
+    const newTab1 = await context.newPage();
+    await newTab1.goto('/');
+
+    const newTab2 = await context.newPage();
+    await newTab2.goto('/');
+
+    const secondContextPage = await secondContext.newPage();
+    await secondContextPage.goto('/');
+
+    await expect(page.locator('text="Welcome back Popol!"')).toBeVisible();
+
+    await page.click('text="Or sign up ?"');
+
+    await expect(page.locator('text="To party sign up !"')).toBeVisible();
+
+    await page.fill('[placeholder="Your nickname"]', internet.userName());
+    await page.fill('[placeholder="Your email"]', internet.email());
+    await page.fill('[placeholder="Your password"]', 'adfg=1435&*&*(SjhgA');
+
+    await page.click('text="Sign Up"');
+
+    await pageIsOnHomeScreen({ page });
+    await pageIsOnHomeScreen({ page: newTab1 });
+    await pageIsOnHomeScreen({ page: newTab2 });
+    await expect(
+        secondContextPage.locator('text="Or sign up ?"'),
+    ).toBeVisible();
+});

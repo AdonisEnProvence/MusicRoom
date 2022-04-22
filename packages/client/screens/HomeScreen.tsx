377c4358e2be6673cf4cedb74af3a4952fa48a0f
Devessier <baptiste@devessier.fr>
2022-04-22 11:42:34 +0200

    feat(client): improve design of home

--- packages/client/screens/HomeScreen.tsx
+++ packages/client/screens/HomeScreen.tsx
@@ -1,28 +1,43 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import { MtvWorkflowState } from '@musicroom/types';
-import { Button, useSx, View } from 'dripsy';
+import { ScrollView, Text, useSx, View } from 'dripsy';
 import { TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import {
-    AppScreen,
-    AppScreenContainer,
-    AppScreenHeader,
-} from '../components/kit';
-import { useMusicPlayerContext } from '../hooks/musicPlayerHooks';
-import { useUserContext } from '../hooks/userHooks';
+import { AppScreenContainer, AppScreenHeader } from '../components/kit';
 import { HomeTabHomeScreenScreenProps } from '../types';
 import AppScreenWithMenu from '../components/kit/AppScreenWithMenu';
 
 const HomeScreen: React.FC<HomeTabHomeScreenScreenProps> = ({ navigation }) => {
     const insets = useSafeAreaInsets();
-    const { sendToMusicPlayerMachine } = useMusicPlayerContext();
-    const { sendToUserMachine } = useUserContext();
     const sx = useSx();
 
     function handleMyProfileIconPress() {
         navigation.navigate('MyProfile', {
             screen: 'MyProfileIndex',
+        });
+    }
+
+    function redirectToTracksSearch() {
+        navigation.navigate('Search', {
+            screen: 'SearchTracks',
+        });
+    }
+
+    function handleCreateMtvRoom() {
+        redirectToTracksSearch();
+    }
+
+    function handleJoinMtvRoom() {
+        navigation.navigate('MusicTrackVoteSearch');
+    }
+
+    function handleCreateMpeRoom() {
+        redirectToTracksSearch();
+    }
+
+    function handleJoinMpeRoom() {
+        navigation.navigate('MusicPlaylistEditorRoomsSearch', {
+            screen: 'MusicPlaylistEditorRoomsSearchModal',
         });
     }
 
@@ -56,92 +71,185 @@ const HomeScreen: React.FC<HomeTabHomeScreenScreenProps> = ({ navigation }) => {
             />
 
             <AppScreenContainer testID="home-screen-container">
-                <Button
-                    title="Go to Music Track Vote"
-                    onPress={() => {
-                        navigation.navigate('MusicTrackVoteSearch');
-                    }}
-                />
+                <ScrollView sx={{ flex: 1 }}>
+                    <View sx={{ marginTop: 'l' }}>
+                        <Text
+                            sx={{
+                                color: 'white',
+                                fontWeight: 'bold',
+                                fontSize: 'l',
+                            }}
+                        >
+                            Welcome to MusicRoom
+                        </Text>
 
-                <Button
-                    title="Go to Music Playlist Editor"
-                    onPress={() => {
-                        navigation.navigate('MusicPlaylistEditorRoomsSearch', {
-                            screen: 'MusicPlaylistEditorRoomsSearchModal',
-                        });
-                    }}
-                />
+                        <Text
+                            sx={{
+                                color: 'white',
+                                marginTop: 'm',
+                                fontSize: 's',
+                            }}
+                        >
+                            Listen to music with your friends or create with
+                            them the music playlist of your next trip.
+                        </Text>
+                    </View>
 
-                <Button
-                    title="Suggest track modal"
-                    onPress={() => {
-                        navigation.navigate('SuggestTrack', {
-                            screen: 'SuggestTrackModal',
-                        });
-                    }}
-                />
-                <Button
-                    title="Ask for geoloc"
-                    onPress={() => {
-                        sendToUserMachine({
-                            type: 'REQUEST_DEDUPLICATE_LOCATION_PERMISSION',
-                        });
-                    }}
-                />
+                    <View sx={{ marginTop: 'xxl' }}>
+                        <Text
+                            sx={{
+                                color: 'white',
+                                fontSize: 'm',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            Music Track Vote
+                        </Text>
 
-                <Button
-                    title="known user profile page"
-                    onPress={() => {
-                        navigation.navigate('UserProfile', {
-                            screen: 'UserProfileIndex',
-                            params: {
-                                userID: '7d21f121-bed5-4c20-9da5-94746bbc8c08',
-                            },
-                        });
-                    }}
-                />
+                        <Text
+                            sx={{
+                                color: 'white',
+                                marginTop: 'm',
+                                fontSize: 's',
+                            }}
+                        >
+                            Create a collaborative listening room for your
+                            parties as well as your global events! Participants
+                            suggest and vote for tracks.
+                        </Text>
 
-                <Button
-                    title="Inject fake room"
-                    onPress={() => {
-                        const fakeState: MtvWorkflowState = {
-                            currentTrack: {
-                                artistName: 'artistName',
-                                title: 'title',
-                                duration: 1000,
-                                elapsed: 1,
-                                score: 1,
-                                id: 'sDNwx9XYWIs',
-                            },
-                            name: 'JUST A FAKE ROOM',
-                            playing: false,
-                            roomCreatorUserID: 'JUST A CREATOR ID',
-                            roomID: 'JUST A ROOM ID',
-                            playingMode: 'BROADCAST',
-                            tracks: null,
-                            minimumScoreToBePlayed: 1,
-                            isOpen: true,
-                            isOpenOnlyInvitedUsersCanVote: false,
-                            hasTimeAndPositionConstraints: true,
-                            timeConstraintIsValid: null,
-                            delegationOwnerUserID: null,
-                            userRelatedInformation: {
-                                hasControlAndDelegationPermission: true,
-                                userFitsPositionConstraint: null,
-                                emittingDeviceID: 'EMITTING DEVICE',
-                                userID: 'JUST A USER ID',
-                                userHasBeenInvited: false,
-                                tracksVotedFor: [],
-                            },
-                            usersLength: 2,
-                        };
+                        <View
+                            sx={{
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                alignItems: 'center',
+                                marginTop: 'l',
+                            }}
+                        >
+                            <TouchableOpacity
+                                style={sx({
+                                    marginRight: 'l',
+                                    marginBottom: 'l',
 
-                        sendToMusicPlayerMachine({
-                            type: 'RETRIEVE_CONTEXT',
-                            state: fakeState,
-                        });
-                    }}
-                />
+                                    backgroundColor: 'secondary',
+                                    paddingX: 's',
+                                    paddingY: 'm',
+                                    borderRadius: 's',
+                                })}
+                                onPress={handleCreateMtvRoom}
+                            >
+                                <Text
+                                    sx={{
+                                        color: 'white',
+                                        fontWeight: 'bold',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    Create a room
+                                </Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity
+                                style={sx({
+                                    marginBottom: 'l',
+
+                                    paddingX: 's',
+                                    paddingY: 'm',
+                                    backgroundColor: 'greyLighter',
+                                    borderRadius: 's',
+                                })}
+                                onPress={handleJoinMtvRoom}
+                            >
+                                <Text
+                                    sx={{
+                                        color: 'greyLight',
+                                        textAlign: 'center',
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    Join a room
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    <View sx={{ marginTop: 'xxl', marginBottom: 'xxl' }}>
+                        <Text
+                            sx={{
+                                color: 'white',
+                                fontSize: 'm',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            Music Playlist Editor
+                        </Text>
+
+                        <Text
+                            sx={{
+                                color: 'white',
+                                marginTop: 'm',
+                                fontSize: 's',
+                            }}
+                        >
+                            List your favorite songs in playlists and ask your
+                            friends to complete them.
+                        </Text>
+
+                        <View
+                            sx={{
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                alignItems: 'center',
+                                marginTop: 'l',
+                            }}
+                        >
+                            <TouchableOpacity
+                                style={sx({
+                                    marginRight: 'l',
+                                    marginBottom: 'l',
+
+                                    backgroundColor: 'secondary',
+                                    paddingX: 's',
+                                    paddingY: 'm',
+                                    borderRadius: 's',
+                                })}
+                                onPress={handleCreateMpeRoom}
+                            >
+                                <Text
+                                    sx={{
+                                        color: 'white',
+                                        fontWeight: 'bold',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    Create a room
+                                </Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity
+                                style={sx({
+                                    marginBottom: 'l',
+
+                                    paddingX: 's',
+                                    paddingY: 'm',
+                                    backgroundColor: 'greyLighter',
+                                    borderRadius: 's',
+                                })}
+                                onPress={handleJoinMpeRoom}
+                            >
+                                <Text
+                                    sx={{
+                                        color: 'greyLight',
+                                        textAlign: 'center',
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    Join a room
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
             </AppScreenContainer>
         </AppScreenWithMenu>
     );

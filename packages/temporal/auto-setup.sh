53a88665fadf35aa96704d5ff43c04a8615aaa9e
Devessier <baptiste@devessier.fr>
2022-04-27 15:02:19 +0200

    build: comment lines about visibility database we won't use

--- packages/temporal/auto-setup.sh
+++ packages/temporal/auto-setup.sh
@@ -9,7 +9,7 @@ SKIP_SCHEMA_SETUP="${SKIP_SCHEMA_SETUP:-false}"
 
 # Cassandra
 KEYSPACE="${KEYSPACE:-temporal}"
-VISIBILITY_KEYSPACE="${VISIBILITY_KEYSPACE:-temporal_visibility}"
+# VISIBILITY_KEYSPACE="${VISIBILITY_KEYSPACE:-temporal_visibility}"
 
 CASSANDRA_SEEDS="${CASSANDRA_SEEDS:-}"
 CASSANDRA_PORT="${CASSANDRA_PORT:-9042}"
@@ -23,7 +23,7 @@ CASSANDRA_REPLICATION_FACTOR="${CASSANDRA_REPLICATION_FACTOR:-1}"
 
 # MySQL/PostgreSQL
 DBNAME="${DBNAME:-temporal}"
-VISIBILITY_DBNAME="${VISIBILITY_DBNAME:-temporal_visibility}"
+# VISIBILITY_DBNAME="${VISIBILITY_DBNAME:-temporal_visibility}"
 DB_PORT="${DB_PORT:-3306}"
 
 MYSQL_SEEDS="${MYSQL_SEEDS:-}"
@@ -45,7 +45,7 @@ ES_PORT="${ES_PORT:-9200}"
 ES_USER="${ES_USER:-}"
 ES_PWD="${ES_PWD:-}"
 ES_VERSION="${ES_VERSION:-v7}"
-ES_VIS_INDEX="${ES_VIS_INDEX:-temporal_visibility_v1_dev}"
+# ES_VIS_INDEX="${ES_VIS_INDEX:-temporal_visibility_v1_dev}"
 ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS="${ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS:-0}"
 
 # Server setup
@@ -131,45 +131,45 @@ wait_for_db() {
 }
 
 setup_cassandra_schema() {
-    # TODO (alex): Remove exports
-    export CASSANDRA_USER=${CASSANDRA_USER}
-    export CASSANDRA_PORT=${CASSANDRA_PORT}
-    export CASSANDRA_ENABLE_TLS=${CASSANDRA_TLS_ENABLED}
-    export CASSANDRA_TLS_CERT=${CASSANDRA_CERT}
-    export CASSANDRA_TLS_KEY=${CASSANDRA_CERT_KEY}
-    export CASSANDRA_TLS_CA=${CASSANDRA_CA}
+    # # TODO (alex): Remove exports
+    # export CASSANDRA_USER=${CASSANDRA_USER}
+    # export CASSANDRA_PORT=${CASSANDRA_PORT}
+    # export CASSANDRA_ENABLE_TLS=${CASSANDRA_TLS_ENABLED}
+    # export CASSANDRA_TLS_CERT=${CASSANDRA_CERT}
+    # export CASSANDRA_TLS_KEY=${CASSANDRA_CERT_KEY}
+    # export CASSANDRA_TLS_CA=${CASSANDRA_CA}
 
-    { export CASSANDRA_PASSWORD=${CASSANDRA_PASSWORD}; } 2> /dev/null
+    # { export CASSANDRA_PASSWORD=${CASSANDRA_PASSWORD}; } 2> /dev/null
 
-    SCHEMA_DIR=${TEMPORAL_HOME}/schema/cassandra/temporal/versioned
-    temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" create -k "${KEYSPACE}" --rf "${CASSANDRA_REPLICATION_FACTOR}"
-    temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${KEYSPACE}" setup-schema -v 0.0
-    temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${KEYSPACE}" update-schema -d "${SCHEMA_DIR}"
+    # SCHEMA_DIR=${TEMPORAL_HOME}/schema/cassandra/temporal/versioned
+    # temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" create -k "${KEYSPACE}" --rf "${CASSANDRA_REPLICATION_FACTOR}"
+    # temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${KEYSPACE}" setup-schema -v 0.0
+    # temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${KEYSPACE}" update-schema -d "${SCHEMA_DIR}"
 
-    VISIBILITY_SCHEMA_DIR=${TEMPORAL_HOME}/schema/cassandra/visibility/versioned
-    temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" create -k "${VISIBILITY_KEYSPACE}" --rf "${CASSANDRA_REPLICATION_FACTOR}"
-    temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${VISIBILITY_KEYSPACE}" setup-schema -v 0.0
-    temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${VISIBILITY_KEYSPACE}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
+    # VISIBILITY_SCHEMA_DIR=${TEMPORAL_HOME}/schema/cassandra/visibility/versioned
+    # temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" create -k "${VISIBILITY_KEYSPACE}" --rf "${CASSANDRA_REPLICATION_FACTOR}"
+    # temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${VISIBILITY_KEYSPACE}" setup-schema -v 0.0
+    # temporal-cassandra-tool --ep "${CASSANDRA_SEEDS}" -k "${VISIBILITY_KEYSPACE}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
 }
 
 setup_mysql_schema() {
-    # TODO (alex): Remove exports
-    { export SQL_PASSWORD=${MYSQL_PWD}; } 2> /dev/null
+    # # TODO (alex): Remove exports
+    # { export SQL_PASSWORD=${MYSQL_PWD}; } 2> /dev/null
 
-    if [ "${MYSQL_TX_ISOLATION_COMPAT}" == "true" ]; then
-        MYSQL_CONNECT_ATTR=(--connect-attributes "tx_isolation=READ-COMMITTED")
-    else
-        MYSQL_CONNECT_ATTR=()
-    fi
+    # if [ "${MYSQL_TX_ISOLATION_COMPAT}" == "true" ]; then
+    #     MYSQL_CONNECT_ATTR=(--connect-attributes "tx_isolation=READ-COMMITTED")
+    # else
+    #     MYSQL_CONNECT_ATTR=()
+    # fi
 
-    SCHEMA_DIR=${TEMPORAL_HOME}/schema/mysql/v57/temporal/versioned
-    temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" create --db "${DBNAME}"
-    temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${DBNAME}" setup-schema -v 0.0
-    temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${DBNAME}" update-schema -d "${SCHEMA_DIR}"
-    VISIBILITY_SCHEMA_DIR=${TEMPORAL_HOME}/schema/mysql/v57/visibility/versioned
-    temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" create --db "${VISIBILITY_DBNAME}"
-    temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${VISIBILITY_DBNAME}" setup-schema -v 0.0
-    temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${VISIBILITY_DBNAME}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
+    # SCHEMA_DIR=${TEMPORAL_HOME}/schema/mysql/v57/temporal/versioned
+    # temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" create --db "${DBNAME}"
+    # temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${DBNAME}" setup-schema -v 0.0
+    # temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${DBNAME}" update-schema -d "${SCHEMA_DIR}"
+    # VISIBILITY_SCHEMA_DIR=${TEMPORAL_HOME}/schema/mysql/v57/visibility/versioned
+    # temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" create --db "${VISIBILITY_DBNAME}"
+    # temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${VISIBILITY_DBNAME}" setup-schema -v 0.0
+    # temporal-sql-tool --ep "${MYSQL_SEEDS}" -u "${MYSQL_USER}" "${MYSQL_CONNECT_ATTR[@]}" --db "${VISIBILITY_DBNAME}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
 }
 
 setup_postgres_schema() {
@@ -184,13 +184,13 @@ setup_postgres_schema() {
     temporal-sql-tool --plugin postgres --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -p "${DB_PORT}" --db "${DBNAME}" setup-schema -v 0.0
     temporal-sql-tool --plugin postgres --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -p "${DB_PORT}" --db "${DBNAME}" update-schema -d "${SCHEMA_DIR}"
 
-    { export SQL_PASSWORD=${VISIBILITY_POSTGRES_PWD}; } 2> /dev/null
-    VISIBILITY_SCHEMA_DIR=${TEMPORAL_HOME}/schema/postgresql/v96/visibility/versioned
-    if [[ "${VISIBILITY_DBNAME}" != "${POSTGRES_USER}" && "${SKIP_POSTGRES_DB_CREATION}" != true ]]; then
-        temporal-sql-tool --plugin postgres --ep "${VISIBILITY_POSTGRES_SEEDS}" -u "${VISIBILITY_POSTGRES_USER}" -p "${VISIBILITY_DB_PORT}" create --db "${VISIBILITY_DBNAME}"
-    fi
-    temporal-sql-tool --plugin postgres --ep "${VISIBILITY_POSTGRES_SEEDS}" -u "${VISIBILITY_POSTGRES_USER}" -p "${VISIBILITY_DB_PORT}" --db "${VISIBILITY_DBNAME}" setup-schema -v 0.0
-    temporal-sql-tool --plugin postgres --ep "${VISIBILITY_POSTGRES_SEEDS}" -u "${VISIBILITY_POSTGRES_USER}" -p "${VISIBILITY_DB_PORT}" --db "${VISIBILITY_DBNAME}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
+    # { export SQL_PASSWORD=${VISIBILITY_POSTGRES_PWD}; } 2> /dev/null
+    # VISIBILITY_SCHEMA_DIR=${TEMPORAL_HOME}/schema/postgresql/v96/visibility/versioned
+    # if [[ "${VISIBILITY_DBNAME}" != "${POSTGRES_USER}" && "${SKIP_POSTGRES_DB_CREATION}" != true ]]; then
+    #     temporal-sql-tool --plugin postgres --ep "${VISIBILITY_POSTGRES_SEEDS}" -u "${VISIBILITY_POSTGRES_USER}" -p "${VISIBILITY_DB_PORT}" create --db "${VISIBILITY_DBNAME}"
+    # fi
+    # temporal-sql-tool --plugin postgres --ep "${VISIBILITY_POSTGRES_SEEDS}" -u "${VISIBILITY_POSTGRES_USER}" -p "${VISIBILITY_DB_PORT}" --db "${VISIBILITY_DBNAME}" setup-schema -v 0.0
+    # temporal-sql-tool --plugin postgres --ep "${VISIBILITY_POSTGRES_SEEDS}" -u "${VISIBILITY_POSTGRES_USER}" -p "${VISIBILITY_DB_PORT}" --db "${VISIBILITY_DBNAME}" update-schema -d "${VISIBILITY_SCHEMA_DIR}"
 }
 
 setup_schema() {
@@ -209,47 +209,47 @@ setup_schema() {
 # === Elasticsearch functions ===
 
 validate_es_env() {
-    if [ "${ENABLE_ES}" == true ]; then
-        if [ -z "${ES_SEEDS}" ]; then
-            echo "ES_SEEDS env must be set if ENABLE_ES is ${ENABLE_ES}"
-            exit 1
-        fi
-    fi
+    # if [ "${ENABLE_ES}" == true ]; then
+    #     if [ -z "${ES_SEEDS}" ]; then
+    #         echo "ES_SEEDS env must be set if ENABLE_ES is ${ENABLE_ES}"
+    #         exit 1
+    #     fi
+    # fi
 }
 
 wait_for_es() {
-    SECONDS=0
+    # SECONDS=0
 
-    ES_SERVER="${ES_SCHEME}://${ES_SEEDS%%,*}:${ES_PORT}"
+    # ES_SERVER="${ES_SCHEME}://${ES_SEEDS%%,*}:${ES_PORT}"
 
-    until curl --silent --fail --user "${ES_USER}":"${ES_PWD}" "${ES_SERVER}" > /dev/null 2>&1; do
-        DURATION=${SECONDS}
+    # until curl --silent --fail --user "${ES_USER}":"${ES_PWD}" "${ES_SERVER}" > /dev/null 2>&1; do
+    #     DURATION=${SECONDS}
 
-        if [ "${ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS}" -gt 0 ] && [ ${DURATION} -ge "${ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS}" ]; then
-            echo 'WARNING: timed out waiting for Elasticsearch to start up. Skipping index creation.'
-            return;
-        fi
+    #     if [ "${ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS}" -gt 0 ] && [ ${DURATION} -ge "${ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS}" ]; then
+    #         echo 'WARNING: timed out waiting for Elasticsearch to start up. Skipping index creation.'
+    #         return;
+    #     fi
 
-        echo 'Waiting for Elasticsearch to start up.'
-        sleep 1
-    done
+    #     echo 'Waiting for Elasticsearch to start up.'
+    #     sleep 1
+    # done
 
-    echo 'Elasticsearch started.'
+    # echo 'Elasticsearch started.'
 }
 
 setup_es_index() {
-    ES_SERVER="${ES_SCHEME}://${ES_SEEDS%%,*}:${ES_PORT}"
-# @@@SNIPSTART setup-es-template-commands
-    # ES_SERVER is the URL of Elasticsearch server i.e. "http://localhost:9200".
-    SETTINGS_URL="${ES_SERVER}/_cluster/settings"
-    SETTINGS_FILE=${TEMPORAL_HOME}/schema/elasticsearch/visibility/cluster_settings_${ES_VERSION}.json
-    TEMPLATE_URL="${ES_SERVER}/_template/temporal_visibility_v1_template"
-    SCHEMA_FILE=${TEMPORAL_HOME}/schema/elasticsearch/visibility/index_template_${ES_VERSION}.json
-    INDEX_URL="${ES_SERVER}/${ES_VIS_INDEX}"
-    curl --fail --user "${ES_USER}":"${ES_PWD}" -X PUT "${SETTINGS_URL}" -H "Content-Type: application/json" --data-binary "@${SETTINGS_FILE}" --write-out "\n"
-    curl --fail --user "${ES_USER}":"${ES_PWD}" -X PUT "${TEMPLATE_URL}" -H 'Content-Type: application/json' --data-binary "@${SCHEMA_FILE}" --write-out "\n"
-    curl --user "${ES_USER}":"${ES_PWD}" -X PUT "${INDEX_URL}" --write-out "\n"
-# @@@SNIPEND
+#     ES_SERVER="${ES_SCHEME}://${ES_SEEDS%%,*}:${ES_PORT}"
+# # @@@SNIPSTART setup-es-template-commands
+#     # ES_SERVER is the URL of Elasticsearch server i.e. "http://localhost:9200".
+#     SETTINGS_URL="${ES_SERVER}/_cluster/settings"
+#     SETTINGS_FILE=${TEMPORAL_HOME}/schema/elasticsearch/visibility/cluster_settings_${ES_VERSION}.json
+#     TEMPLATE_URL="${ES_SERVER}/_template/temporal_visibility_v1_template"
+#     SCHEMA_FILE=${TEMPORAL_HOME}/schema/elasticsearch/visibility/index_template_${ES_VERSION}.json
+#     INDEX_URL="${ES_SERVER}/${ES_VIS_INDEX}"
+#     curl --fail --user "${ES_USER}":"${ES_PWD}" -X PUT "${SETTINGS_URL}" -H "Content-Type: application/json" --data-binary "@${SETTINGS_FILE}" --write-out "\n"
+#     curl --fail --user "${ES_USER}":"${ES_PWD}" -X PUT "${TEMPLATE_URL}" -H 'Content-Type: application/json' --data-binary "@${SCHEMA_FILE}" --write-out "\n"
+#     curl --user "${ES_USER}":"${ES_PWD}" -X PUT "${INDEX_URL}" --write-out "\n"
+# # @@@SNIPEND
 }
 
 # === Server setup ===
